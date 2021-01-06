1a360763c87eb50fdbbdb5e56e9801425a2cc110
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2021-01-06 15:02:07 -0300

    First commit

---
+++ src/pages/Components/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import '../../App.css';
+
+class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currency: 'BRL',
+    };
+  }
+
+  render() {
+    const { currency } = this.state;
+    const { email, total } = this.props;
+    return (
+      <header>
+        <div>TrybeWallet</div>
+        <div data-testid="email-field">
+          {`User: ${email}`}
+        </div>
+        <div data-testid="total-field">
+          Outgoing:
+          {total > 0 ? total.toFixed(2) : 0}
+        </div>
+        <div data-testid="header-currency-field">
+          {`Currency: ${currency}`}
+        </div>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  total: state.wallet.total,
+});
+
+export default connect(mapStateToProps)(Header);
+
+Header.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
