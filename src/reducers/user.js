850d998c984940fb9b8aae9e9f5844c48a432a92
Felipe Nascimento <flpnascto@gmail.com>
2021-01-06 18:23:00 -0300

    Definir user reducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,15 @@
+// import { act } from "@testing-library/react";
+
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import INITIAL_STATE from './initialState';
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case 'LOGIN':
+    return { ...state, user: { ...state.user, email: action.payload } };
+  default:
+    return state;
+  }
+};
+
+export default user;
