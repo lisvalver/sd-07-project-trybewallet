c59c609c6d7d6011c9c42cb6a7ff355a8567b7b7
vanderson-henrique <vhbenedito@gmail.com>
2021-01-06 15:33:05 -0300

    Requisito 5 - Tabela de gastos

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import FormWallet from '../components/FormWallet';
-// import ExpenseTable from '../components/ExpenseTable';
+import ExpenseTable from '../components/ExpenseTable';
 
 class Wallet extends React.Component {
   render() {
@@ -9,6 +9,7 @@ class Wallet extends React.Component {
       <div>
         <Header />
         <FormWallet />
+        <ExpenseTable />
       </div>
     );
   }
