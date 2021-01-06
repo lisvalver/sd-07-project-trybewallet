61a521313fa6bddf1072905a402d8ba347ccfe0d
TIAGO <tiagoesdrasvieira@gmail.com>
2021-01-06 18:52:34 -0300

    Requisito 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Header extends React.Component {
   render() {
     const { user } = this.props;
     return (
-      <div>
-        <header data-testid="email-field">
-          Email: { user }
-        </header>
-      </div>
+      <header>
+        <div data-testid="email-field">
+          Email:
+          { user }
+        </div>
+        <div>
+          Despesa Total: R$
+          <span data-testid="total-field">
+            0
+          </span>
+        </div>
+        <div data-testid="header-currency-field">
+          BRL
+        </div>
+      </header>
     );
   }
 }
+
+Header.propTypes = {
+  user: PropTypes.string.isRequired,
+};
 
 export default Header;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Wallet extends React.Component {
@@ -16,5 +17,9 @@ class Wallet extends React.Component {
 const mapStateToProps = (state) => ({
   user: state.user.email,
 });
+
+Wallet.propTypes = {
+  user: PropTypes.string.isRequired,
+};
 
 export default connect(mapStateToProps)(Wallet);
