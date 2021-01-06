34b1bfbef55b6e426235bf7c482a26216d51f49d
vanderson-henrique <vhbenedito@gmail.com>
2021-01-06 14:59:47 -0300

    Requisitos 1 e 2 - Configuração da página de login e rotas

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,77 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import {
+  CURRENCY_QUERY,
+  ADD_EXPENSES,
+  DELETE_EXPENSE,
+  EDIT_EXPENSE,
+  EDITING,
+} from '../actions';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+  inEditing: {
+    edit: false,
+    expenseEdit: {},
+  },
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case CURRENCY_QUERY:
+    return { ...state, currencies: action.currencies };
+  case ADD_EXPENSES:
+    return {
+      ...state,
+      expenses: [
+        ...state.expenses,
+        {
+          id: state.expenses.length,
+          value: action.expense.value,
+          description: action.expense.description,
+          currency: action.expense.currency,
+          method: action.expense.method,
+          tag: action.expense.tag,
+          exchangeRates: action.expense.exchangeRates,
+        },
+      ],
+    };
+  case DELETE_EXPENSE:
+    return {
+      ...state,
+      expenses: state.expenses.filter((expense) => expense.id !== action.id),
+    };
+  case EDIT_EXPENSE:
+    return {
+      ...state,
+      inEditing: {
+        edit: !state.inEditing.edit,
+        expenseEdit: action.expenseEdit,
+      },
+    };
+  case EDITING:
+    return {
+      ...state,
+      expenses: [
+        ...state.expenses.map((expense) => {
+          if (expense.id !== action.expenseEdit.id) {
+            return expense;
+          } return (
+            { id: action.expenseEdit.id,
+              value: action.newData.value,
+              description: action.newData.description,
+              currency: action.newData.currency,
+              method: action.newData.method,
+              tag: action.newData.tag,
+              exchangeRates: action.expenseEdit.exchangeRates,
+            }
+          );
+        }),
+      ],
+    };
+  default:
+    return state;
+  }
+};
+
+export default wallet;
