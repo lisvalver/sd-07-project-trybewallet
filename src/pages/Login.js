aa3a51eb7d8f3da0c9e3172f182b3c40a7828494
Strongreen <strongreen@live.com>
2021-01-06 17:50:41 -0300

    Tela de Login e primeiras configuraçoes do redux

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,35 +5,63 @@ import './login.css';
 class Login extends React.Component {
   render() {
     return (
-    <div className="login">
-      <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css" />
-      <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-      <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-      <div class="container">
-        <div class="card card-container">
-        <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+      <div className="login">
+        <link
+          href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css"
+          rel="stylesheet"
+          id="bootstrap-css"
+        />
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+        <div class="container">
+          <div class="card card-container">
+            <img
+              id="profile-img"
+              class="profile-img-card"
+              src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+            />
             <form class="form-signin">
-                <input type="email" id="email" class="form-control" placeholder="email" data-testid="email-input" required autofocus />
-                <input type="password" id="senha" class="form-control" placeholder="Senha" data-testid="password-input" required />
-                <div id="remember" class="checkbox">
-                    <label>
-                        <input type="checkbox" value="remember-me" /> Lembrar login?
-                    </label>
-                </div>
-                <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Entrar</button>
+              <input
+                type="email"
+                id="email"
+                class="form-control"
+                placeholder="email"
+                data-testid="email-input"
+                required
+                autofocus
+              />
+              <input
+                type="password"
+                id="senha"
+                class="form-control"
+                placeholder="Senha"
+                data-testid="password-input"
+                required
+              />
+              <div id="remember" class="checkbox">
+                <label>
+                  <input type="checkbox" value="remember-me" /> Lembrar login?
+                </label>
+              </div>
+              <button
+                class="btn btn-lg btn-primary btn-block btn-signin"
+                type="submit"
+              >
+                Entrar
+              </button>
             </form>
             <Link href="#" class="forgot-password">
-                Esqueceu a senha?
+              Esqueceu a senha?
             </Link>
+          </div>
         </div>
-    </div>
-    </div>);
+      </div>
+    );
   }
 }
 
 export default Login;
 
-
-// Caso logue 
+// Caso logue
 // Mudar o avatar
 // <img class="profile-img-card" src="//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120" alt="" />
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 
 class notfound extends Component {
-    render() {
-        return (
-            <div>
-                Essa Página não existe!
-            </div>
-        );
-    }
+  render() {
+    return <div>Essa Página não existe!</div>;
+  }
 }
 
 export default notfound;
