7bcdbbaf6bf205a3800e5858bfdfe61ce52fd857
Felipe Nascimento <flpnascto@gmail.com>
2021-01-06 15:12:16 -0300

    Rota para página Login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div>
+      Hello, TrybeWallet!
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
