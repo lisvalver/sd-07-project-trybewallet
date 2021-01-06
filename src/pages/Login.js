c7fd41d006523928a90b2192233ee347b915510c
Felipe Nascimento <flpnascto@gmail.com>
2021-01-06 15:53:56 -0300

    Criado estados para email e senha

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,43 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChangeInput = this.handleChangeInput.bind(this);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+  }
+
+  handleChangeInput(field, newValue) {
+    this.setState({ [field]: newValue });
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
       <div>
         Login-Logo
-        <input type="text" data-testid="email-input" placeholder="Insira seu email" />
-        <input type="password" data-testid="password-input" placeholder="senha" />
+        <input
+          type="text"
+          data-testid="email-input"
+          placeholder="Insira seu email"
+          value={ email }
+          onChange={ (e) => this.handleChangeInput('email', e.target.value) }
+        />
+
+        <input
+          type="password"
+          data-testid="password-input"
+          placeholder="senha"
+          value={ password }
+          onChange={ (e) => this.handleChangeInput('password', e.target.value) }
+        />
+
         <button type="button">Entrar</button>
       </div>
     );
