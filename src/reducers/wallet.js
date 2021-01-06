548ac4999ba82ac1aeed8fdb062be8a3003c1668
lucasvr39 <lucasveras1994@gmail.com>
2021-01-06 15:45:17 -0300

    implement all project from my project when I was in sd 06

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,36 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { DELETE_EXPENSE, EDIT_EXPENSE, SAVE_EXPENSE } from '../actions';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+export default function wallet(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case SAVE_EXPENSE:
+    return {
+      ...state,
+      expenses: [...state.expenses, action.expenses],
+    };
+  case DELETE_EXPENSE:
+    return {
+      ...state,
+      expenses: [
+        ...state.expenses.filter((expense) => expense.id !== action.deleteExpense.id),
+      ],
+    };
+  case EDIT_EXPENSE:
+    return {
+      ...state,
+      expenses: state.expenses.map((expense) => {
+        if (expense.id === action.editedExpense.id) {
+          return { ...expense, ...action.editedExpense };
+        }
+        return expense;
+      }),
+    };
+  default:
+    return state;
+  }
+}
