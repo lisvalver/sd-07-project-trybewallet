2890044bf53cc377b04b38b1b46a331483a5d6fb
Pedro Marques Fr <pedromarques1011@gmail.com>
2021-01-06 15:19:52 -0300

    table done

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Table, Button } from 'react-bootstrap';
 import { actionDelete, actionChangeEditState, idBeingEdited } from '../actions';
 
 function TableComp({
@@ -15,67 +14,66 @@ function TableComp({
   //   };
   return (
     <div>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
-        </thead>
-        <tbody>
-          {expenses.map(
-            ({
-              id,
-              currency,
-              value,
-              description,
-              method,
-              tag,
-              exchangeRates,
-            }) => (
-              <tr key={ id }>
-                <td>{description}</td>
-                <td>{tag}</td>
-                <td>{method}</td>
-                <td>{value}</td>
-                <td>{exchangeRates[currency].name}</td>
-                <td>{parseFloat(exchangeRates[currency].ask).toFixed(2)}</td>
-                <td>
-                  {parseFloat(value * exchangeRates[currency].ask).toFixed(2)}
-                </td>
-                <td>Real</td>
-                <td>
-                  <Button
-                    variant="warning"
-                    data-testid="edit-btn"
-                    onClick={ () => {
-                      edit();
-                      editStatus(id);
-                    } }
-                  >
-                    Editar
-                  </Button>
-                  {' '}
-                  <Button
-                    variant="danger"
-                    data-testid="delete-btn"
-                    onClick={ () => deleteA(id) }
-                  >
-                    Remover
-                  </Button>
-                </td>
-              </tr>
-            ),
-          )}
-        </tbody>
-      </Table>
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+      </thead>
+      <tbody>
+        {expenses.map(
+          ({
+            id,
+            currency,
+            value,
+            description,
+            method,
+            tag,
+            exchangeRates,
+          }) => (
+            <tr key={ id }>
+              <td>{description}</td>
+              <td>{tag}</td>
+              <td>{method}</td>
+              <td>{value}</td>
+              <td>{exchangeRates[currency].name}</td>
+              <td>{parseFloat(exchangeRates[currency].ask).toFixed(2)}</td>
+              <td>
+                {parseFloat(value * exchangeRates[currency].ask).toFixed(2)}
+              </td>
+              <td>Real</td>
+              <td>
+                <button
+                  data-testid="edit-btn"
+                  type="button"
+                  onClick={ () => {
+                    edit();
+                    editStatus(id);
+                  } }
+                >
+                  Editar
+                </button>
+                {' '}
+                <button
+                  data-testid="delete-btn"
+                  type="button"
+                  onClick={ () => deleteA(id) }
+                >
+                  Remover
+                </button>
+              </td>
+            </tr>
+          ),
+        )}
+      </tbody>
+
     </div>
   );
 }
