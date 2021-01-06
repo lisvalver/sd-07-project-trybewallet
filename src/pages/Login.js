0ea512c6336227a90266ef33c17ca4d17e14385f
Macarena Rodriguez <macarenar697@gmail.com>
2021-01-06 17:31:46 -0300

    Criation of input and button, verification of emal and password

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,62 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.verifyInput = this.verifyInput.bind(this);
+    this.state = {
+      email: '',
+      password: '',
+      disableButton: true,
+    };
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      [target.name]: target.value,
+    }, () => this.verifyInput());
+  }
+
+  //  https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+  verifyInput() {
+    const { email, password } = this.state;
+    const regEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.+-]+\.com$/;
+    const num = 5;
+    if (regEx.test(email) && password.length > num) {
+      console.log('ksdjk');
+      this.setState({
+        disableButton: false,
+      });
+    } else {
+      this.setState({
+        disableButton: true,
+      });
+    }
+  }
+
   render() {
-    return <div>Login</div>;
+    const { disableButton } = this.state;
+    return (
+      <div>
+        <input
+          data-testid="email-input"
+          name="email"
+          onChange={ this.handleChange }
+        />
+        <input
+          data-testid="password-input"
+          name="password"
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          disabled={ disableButton }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
