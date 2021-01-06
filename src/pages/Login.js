d59f53797e70b1456c0dc80593567edcbb42e0ca
alvaro <alvarovascon2@gmail.com>
2021-01-06 17:19:19 -0300

    validaçao email e senha

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,29 +1,67 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom'
+import { Redirect } from 'react-router-dom';
 import store from '../store'
 
 class Login extends React.Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      redirect: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+  handleClick() {
     const { email, password } = this.state;
-    console.log(email, store)
+    let validPassword;
+    const minLength = 6;
+    const validEmail = email.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/);
+    if (password.length >= 6) {
+      validPassword = true;
+    }
+    if (validEmail && validPassword) {
+      this.setState({ redirect: true});
+      return;
+    }
+    alert('e-mail ou senha inválidos!');
+  }
+
+  render() {
+    const { email, password, redirect } = this.state;
+
+    if (redirect) {
+      return <Redirect to="/carteira" />
+    }
+    
     return (
       <div>
         <input
           type="email"
+          name="email"
           data-testid="email-input"
           placeholder="e-mail"
           value={ email }
-          onChange={}
+          onChange={this.handleChange}
         />
         <input
           type="password"
+          name="password"
           data-testid="password-input"
           placeholder="senha"
           value={ password }
-          onChange={}
+          onChange={this.handleChange}
         />
-        <Link to="/carteira">Entrar</Link>
+        <button type="button" onClick={ this.handleClick }>Entrar</button>
       </div>
     );
   }
