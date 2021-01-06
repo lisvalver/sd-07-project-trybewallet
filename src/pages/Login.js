bff0b04da860cd9040f1858e926cffe450b73366
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-01-06 17:12:12 -0300

    Criando pagina de login controlado pelo componente e colocando regex para validar email e senha

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,79 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+    this.verifyEmailAndPassword = this.verifyEmailAndPassword.bind(this);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  verifyEmailAndPassword() {
+    const { email, password } = this.state;
+
+    const regexEmail = new RegExp('.+@[A-z]+[.]com');
+    const regexPassword = new RegExp('.{6}');
+
+    if (regexEmail.test(email) && regexPassword.test(password)) {
+      return true;
+    }
+  }
+
+  buttonDisabled() {
+    return (
+      <button type="button" disabled="disabled">
+        Entrar
+      </button>
+    );
+  }
+
+  buttonEnabled() {
+    return <button type="button">Entrar</button>;
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+
+    // prettier-ignore
+    return (
+      <div className="App">
+        Login - Trybe Wallets
+        <form>
+          <input
+            data-testid="email-input"
+            type="email"
+            placeholder="E-mail"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+          <br />
+          <input
+            data-testid="password-input"
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
+          />
+          <br />
+          {this.verifyEmailAndPassword() ? this.buttonEnabled() : this.buttonDisabled()}
+        </form>
+      </div>
+    );
   }
 }
 
