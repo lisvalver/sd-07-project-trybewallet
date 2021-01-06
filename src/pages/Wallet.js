528286b4a6152bcf6af550ec13fa504084aa3a0c
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-01-06 18:08:17 -0300

    adicionando header e campos email, despesa e cambio em Wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+
+    return (
+    <div>
+      <header>
+        <h2 data-testid="email-field">Email: {email}</h2>
+        <h2 data-testid="total-field">Despesa total: 0</h2>
+        <h2 data-testid="header-currency-field">Câmbio: BRL</h2>
+      </header>
+    </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = state => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+}
+
+export default connect(mapStateToProps)(Wallet);
