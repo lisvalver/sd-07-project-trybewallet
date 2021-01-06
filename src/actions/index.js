c35abf7d6586ef4e06000559e6d426f359c1bef2
Bruno <bruno_grb@hotmail.com>
2021-01-06 17:28:54 -0300

    corrigindo eslint

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,6 @@ export const ADD_USER = 'ADD_USER';
 
 export const ADD_WALLET = 'ADD_WALLET';
 
-const addUser = (user) => ({ type: 'ADD_USER', user });
+// const addUser = (user) => ({ type: 'ADD_USER', user });
 
-const addWallet = (wallet) => ({ type: 'ADD_WALLET', wallet });
+// const addWallet = (wallet) => ({ type: 'ADD_WALLET', wallet });
