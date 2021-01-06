c2b7c1ab34811d28e5fd2076f50a2eef6d85bc8a
Pedro Marques Fr <pedromarques1011@gmail.com>
2021-01-06 15:24:18 -0300

    done store index

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,12 @@
 import { createStore, applyMiddleware } from 'redux';
 // importe o redux-thunk
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from '../reducers';
 
 // aplique o middleware
 const store = createStore(
   reducers,
-  composeWithDevTools(applyMiddleware(thunk)),
+  applyMiddleware(thunk),
 );
 
 export default store;
