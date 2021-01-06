a6638f01fbcbb1622206231e6600fc348ef381e0
Lorena Góes <goesloren@gmail.com>
2021-01-06 16:03:43 -0300

    Criando estrutura HTML

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Login extends React.Component {
+class Login extends Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <label htmlFor="title-login">TRYBE</label>   
+        <fieldset>
+          <input
+            type="email"
+            name="email"
+            // value={}
+            // onchange={}
+            placeholder="hermione@trybe.com"
+            data-testid="email-input"
+          />
+          <input
+            type="password"
+            name="password"
+            // value={}
+            // onchange={}
+            placeholder="Senha"
+            data-testid="password-input"
+          />
+          <button
+            type="submit"
+            // onClick={}
+          >
+            Entrar
+          </button>
+        </fieldset>
+      </form>
+    );
   }
 }
+
+// mapStateToProps = (state) => {
+
+// }
 
 export default Login;
