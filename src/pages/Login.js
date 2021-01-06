19280dd3fe3f2a0447451d10df7b572da614961d
Felipe Nascimento <flpnascto@gmail.com>
2021-01-06 15:18:58 -0300

    Adicionado input de e-mail e senha e botão entrar na página Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        Login-Logo
+        <input type="text" data-testid="email-input" placeholder="Insira seu email" />
+        <input type="password" data-testid="password-input" placeholder="senha" />
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
