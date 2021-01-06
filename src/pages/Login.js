8bf3ba763a96bdb6c06739d810f13677d68e0736
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-01-06 15:00:36 -0300

    Primeiro commit login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return <div>
+      <h1>Trybe Wallet</h1>
+      <input type="text" data-testid="email-input" />
+      <input type="password" data-testid="password-input" />
+    </div>;
   }
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,14 @@
-// import user from './user';
-// import wallet from './wallet';
+import user from './user';
+import wallet from './wallet';
 
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: []
+  }
+}
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
