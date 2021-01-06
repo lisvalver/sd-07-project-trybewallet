51951fe1aa01c7fcacf0c5ed63a2eb9619592f35
Fernando <nando.neves@gmail.com>
2021-01-06 19:35:59 -0300

    third

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,8 +7,10 @@ class Login extends React.Component {
   constructor(props) {
     super(props);
     this.buttonClick = this.buttonClick.bind(this);
+    this.enableDisable = this.enableDisable.bind(this);
     this.state = {
       email: '',
+      password: '',
     };
   }
 
@@ -16,6 +18,15 @@ class Login extends React.Component {
     const { email, history } = this.props;
     history.push('/carteira');
     console.log(email);
+  }
+
+  enableDisable() {
+    const { email, password } = this.state;
+    console.log(password);
+    const regEx = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    const seis = 6;
+    const button = document.querySelector('button');
+    button.disabled = !(regEx.test(email) && password.length > seis);
   }
 
   render() {
@@ -28,7 +39,11 @@ class Login extends React.Component {
           <label htmlFor="email">
             Email
             <input
-              onChange={ ({ target }) => this.setState({ email: target.value }) }
+              onChange={ ({ target }) => {
+                this.setState({
+                  email: target.value,
+                }, this.enableDisable());
+              } }
               className="email"
               data-testid="email-input"
               id="email"
@@ -45,10 +60,20 @@ class Login extends React.Component {
               type="password"
               name="password"
               id="password"
+              onChange={ ({ target }) => {
+                this.setState({
+                  password: target.value,
+                }, this.enableDisable());
+              } }
             />
           </label>
-
-          <button type="button" onClick={ () => this.buttonClick() }>Entrar</button>
+          <button
+            type="button"
+            disabled="true"
+            onClick={ () => this.buttonClick() }
+          >
+            Entrar
+          </button>
         </form>
       </div>);
   }
