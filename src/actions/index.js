d1f8962921454d1c3b016bf9b19adfb61600076a
danielmlduarte <danielduarte.tryber@gmail.com>
2021-01-06 17:12:23 -0300

    Commit inicial - Setup do redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,27 @@
-// Coloque aqui suas actions
+export const REQUEST_PROJECTS = 'REQUEST_PROJECTS';
+export const REQUEST_FAILED = 'REQUEST_FAILED';
+export const LIST_PROJECTS = 'LIST_PROJECTS';
+
+export const addExpense = (expense) => ({
+  type: ADD_EXPENSE,
+  payload: expense,
+});
+
+export const removeExpense = (expense) => ({
+  type: REMOVE_EXPENSE,
+  payload: expense,
+});
+
+export const requestCurrencies = () => ({
+  type: REQUEST_PROJECTS,
+});
+
+export const failedRequest = (error) => ({
+  type: REQUEST_FAILED,
+  error,
+});
+
+export const listProjects = (list) => ({
+  type: LIST_PROJECTS,
+  list,
+});
