15ad9ec8b1e1d75020e7b3373299a385328bb085
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2021-01-06 15:14:23 -0300

    Ajustando a imortação do CSS - Semantic UI

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import './index.css';
-import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
