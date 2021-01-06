eaa43d33ded9167f58e3e7cf7be501c60e8d59a5
Strongreen <strongreen@live.com>
2021-01-06 19:07:03 -0300

    Correçoes do lint

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,48 +4,38 @@ import { Link } from 'react-router-dom';
 class Login extends React.Component {
   render() {
     return (
-          <div>
-            <img
-              id="profile-img"
-              src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-            />
-            <form class="form-signin">
-              <input
-                type="email"
-                id="email"
-                placeholder="email"
-                data-testid="email-input"
-                required
-                autofocus
-              />
-              <input
-                type="password"
-                id="senha"
-                placeholder="Senha"
-                data-testid="password-input"
-                required
-              />
-              <div id="remember">
-                <label>
-                  <input type="checkbox" value="remember-me" /> Lembrar login?
-                </label>
-              </div>
-              <button
-                type="submit"
-              >
-                Entrar
-              </button>
-            </form>
-            <Link href="#">
-              Esqueceu a senha?
-            </Link>
+      <div>
+        <img
+          id="profile-img"
+          src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+        />
+        <form class="form-signin">
+          <input
+            type="email"
+            id="email"
+            placeholder="email"
+            data-testid="email-input"
+            required
+            autofocus
+          />
+          <input
+            type="password"
+            id="senha"
+            placeholder="Senha"
+            data-testid="password-input"
+            required
+          />
+          <div id="remember">
+            <label>
+              <input type="checkbox" value="remember-me" /> Lembrar login?
+            </label>
           </div>
+          <button type="submit">Entrar</button>
+        </form>
+        <Link href="#">Esqueceu a senha?</Link>
+      </div>
     );
   }
 }
 
 export default Login;
-
-// Caso logue
-// Mudar o avatar
-// <img class="profile-img-card" src="//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120" alt="" />
