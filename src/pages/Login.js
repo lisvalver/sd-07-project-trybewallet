7e92661ce0b658292de72dbc58f02a182858fdca
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-01-06 16:29:06 -0300

    Requisito 1 Parcialmente completo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,53 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      validate: true,
+    };
+    this.handleChanger = this.handleChanger.bind(this);
+    this.validateEmail = this.validateEmail.bind(this);
+  }
+
+  async handleChanger({ target: { name, value } }) {
+    await this.setState({ [name]: value });
+    this.validateEmail();
+  }
+
+  validateEmail() {
+    const magicNumber = 5;
+    const { email, password } = this.state;
+    if (email.match(/\S+@\S+\.\S+/) && password.length > magicNumber) {
+      return this.setState({ validate: false });
+    }
+    this.setState({ validate: true });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, validate } = this.state;
+    return (
+      <div>
+        <input
+          data-testid="email-input"
+          type="email"
+          name="email"
+          value={ email }
+          placeholder="email"
+          onChange={ (e) => this.handleChanger(e) }
+        />
+        <input
+          data-testid="password-input"
+          type="text"
+          name="password"
+          value={ password }
+          placeholder="senha"
+          onChange={ (e) => this.handleChanger(e) }
+        />
+        <input type="button" value="Entrar" disabled={ validate } />
+      </div>);
   }
 }
 
