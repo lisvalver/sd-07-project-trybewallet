0d7090d580f65062d08d1c4e3a2fba48e59d4831
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2021-01-06 14:30:55 -0300

    Adicionando Services - API e DATA

---
+++ src/services/data.js
@@ -0,0 +1,13 @@
+export const optionsMethod = [
+  'Dinheiro',
+  'Cartão de crédito',
+  'Cartão de débito',
+];
+
+export const optionsTag = [
+  'Alimentação',
+  'Lazer',
+  'Trabalho',
+  'Transporte',
+  'Saúde',
+];
