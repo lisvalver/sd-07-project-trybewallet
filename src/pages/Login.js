2a8b5f54674bb988fbaa38b2351ae908ebad928d
marcos <marcos.1998226@gmail.com>
2021-01-06 20:20:41 -0300

    iniciando o projeto

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import emailOk from '../helpers/emailValidation';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  handleChangeEmail(event){
+    this.setState({
+      email: event.target.value
+    });
+  }
+
+  handleChangePassword(event){
+    this.setState({
+      password: event.target.value
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+  const { email, password } = this.state;
+    return (
+      <div className="pai">
+        <header>Login</header> const { email, password } = this.state;
+        <label htmlFor="email">
+          E-mail:
+          <input type="text" id="email" data-testid="email-input" onChange={ this.handleChangeEmail }/>
+        </label>
+        <label htmlFor="senha">
+          Senha:
+          <input type="password" id="senha" data-testid="password-input" onChange={ this.handleChangePassword } />
+        </label>
+        <button data-testid="password" type="button">Entrar</button>
+      </div>
+    );
   }
 }
-
+emailOk()
 export default Login;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,4 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+// export function name(params) {
+    
+// }
