253fc4cdbb6eec9bbc99b9b57bcc12b8e256830b
Strongreen <strongreen@live.com>
2021-01-06 18:03:31 -0300

    Correçoes do lint

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -44,7 +44,7 @@ class Login extends React.Component {
                 </label>
               </div>
               <button
-                class="btn btn-lg btn-primary btn-block btn-signin"
+                class="btn btn-lg btn-primary "
                 type="submit"
               >
                 Entrar
