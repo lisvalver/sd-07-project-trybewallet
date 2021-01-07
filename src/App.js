2334c06d7be2cd75dafca4c8222d5c7bcf6137c2
Samanta Below <samanta.below@gmail.com>
2021-01-07 09:25:53 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Provider } from 'react-redux';
 import { Route } from 'react-router-dom';
 import store from './store';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <div>
       <Provider store={ store }>
+        <Route path="/carteira" component={ Wallet } />
         <Route exact path="/" component={ Login } />
       </Provider>
     </div>
