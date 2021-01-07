ae471a1ed5b754985651155238e29fd7f7509800
moacyrrln <moacyrrln@gmail.com>
2021-01-07 12:12:19 -0300

    1212

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 
-
 class App extends React.Component {
   render() {
     return (
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,65 +1,78 @@
 import React from 'react';
 
 class Login extends React.Component {
-
   constructor() {
     super();
     this.state = {
       email: '',
-      password: '',
       emailError: '',
       passwordError: '',
-      passwordlength: '',
-    }
+    };
   }
 
-  passwordChange = event => {
-    this.setState({
-      password: event.target.value,
-      passwordlength: event.target.value.length,
-    })
+  passwordChange(event) {
+    // this.setState({
+    //   password: event.target.value,
+    //   passwordlength: event.target.value.length,
+    // });
 
-    if (event.target.value.length < 6) {
+    const pwl = 6;
+
+    if (event.target.value.length < pwl) {
       this.setState({
-        passwordError: 'A senha tem que possuir mais de 6 caracteres'
-      })
+        passwordError: 'A senha tem que possuir mais de 6 caracteres',
+      });
     } else {
       this.setState({
-        passwordError: 'Senha OK'
-      })
+        passwordError: 'Senha OK',
+      });
     }
   }
 
-  emailChange = event => {
+  emailChange(event) {
+    const { email } = this.state;
     this.setState({
       email: event.target.value,
-      emailError: 'Email OK'
-    })
-    if (this.state.email === '') {
+      emailError: 'Email OK',
+    });
+    if (email === '') {
       this.setState({
-        emailError: 'Insira um email'
-      })
+        emailError: 'Insira um email',
+      });
     } else {
-      const pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-      if (!pattern.test(this.state.email)) {
+      const pattern = new RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.+-]+\.com$/);
+      if (!pattern.test(email)) {
         this.setState({
-          emailError: 'Insira um email válido'
-        })
+          emailError: 'Insira um email válido',
+        });
       }
     }
   }
 
   render() {
+    const { emailError, passwordError } = this.state;
     return (
       <div>
-        <input name="email" type="text" placeholder="email" data-testid="email-input" onChange={this.emailChange} />
-        <input name="password" type="text" placeholder="senha" data-testid="password-input" onChange={this.passwordChange}/>
-        <button>Entrar</button>
+        <input
+          name="email"
+          type="text"
+          placeholder="email"
+          data-testid="email-input"
+          onChange={ this.emailChange }
+        />
+        <input
+          name="password"
+          type="text"
+          placeholder="senha"
+          data-testid="password-input"
+          onChange={ this.passwordChange }
+        />
+        <button type="button">Entrar</button>
         <div>
-          {this.state.emailError}
+          { emailError }
         </div>
         <div>
-          {this.state.passwordError}
+          { passwordError }
         </div>
       </div>
     );
