d2a0c67234a3a6b7e641603db218720ccefa2f59
Fernando Antunes <fernando.antunes1@gmail.com>
2021-01-07 10:07:48 -0300

    Requisito 3 finalizado

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Header extends React.Component {
   render() {
     const { email } = this.props;
+    const totalExpenses = 0;
+    const atualExchange = 'BRL';
     return (
-    <div>      
-      <span data-testid='email-field'>
-        Usuário: { email }
-      </span>
-      
-    </div>
+      <div>
+        <span data-testid="email-field">
+          Usuário:
+          {email}
+        </span>
+        <span data-testid="total-field">
+          Total das despesas:
+          {totalExpenses}
+        </span>
+        <span data-testid="header-currency-field">
+          Câmbio atual:
+          {atualExchange}
+        </span>
+      </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  email: state.user.email
-})
+  email: state.user.email,
+});
 
 export default connect(mapStateToProps)(Header);
+
+Header.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Header from '../components/header'
+import Header from '../components/header';
 
 class Wallet extends React.Component {
   render() {
     return (
-    <div>
-      TrybeWallet
-      <Header />
-    </div>
-      );
+      <div>
+        TrybeWallet
+        <Header />
+      </div>
+    );
   }
 }
 
