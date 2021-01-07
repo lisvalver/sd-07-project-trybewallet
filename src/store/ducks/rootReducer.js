799b1caffeec55ed3e4f40bdbf638fbc348d6679
Lorena Góes <goesloren@gmail.com>
2021-01-07 19:19:40 -0300

    Renomeando reducers

--- src/store/ducks/rootReducer.js
+++ src/store/ducks/rootReducer.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
 
-import user from './user';
-import wallet from './wallet';
+import userReducer from './user';
+import walletReducer from './wallet';
 
-const rootReducer = combineReducers({ user, wallet });
+const rootReducer = combineReducers({ userReducer, walletReducer });
 
 export default rootReducer;
