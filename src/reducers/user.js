c9fa4d06835f169e3ea05b597d6b7e3c1c6b3fa1
Felipe Nascimento <flpnascto@gmail.com>
2021-01-07 00:36:29 -0300

    Ajustar initial State do reducer user

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,12 +1,14 @@
 // import { act } from "@testing-library/react";
 
 // Esse reducer será responsável por tratar as informações da pessoa usuária
-import INITIAL_STATE from './initialState';
+const INITIAL_STATE = {
+  email: '',
+};
 
 const user = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case 'LOGIN':
-    return { ...state, user: { ...state.user, email: action.payload } };
+    return { ...state, email: action.payload };
   default:
     return state;
   }
