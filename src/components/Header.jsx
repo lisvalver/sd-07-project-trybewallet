a49bf2260b7cdc755502a76eb29889c109464371
Felipe Nascimento <flpnascto@gmail.com>
2021-01-07 15:06:31 -0300

    Componente Header para Wallet

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Carteira</h1>
+        <span>
+          {`E-mail:`}
+        </span>
+      </div>
+    );
+  }
+}
+
+export default Header;
