6e36f0edaf5ce7252608ab7f8ff4d7109c1c85e6
Jr3564 <rodolfojr94@gmail.com>
2021-01-07 20:24:41 -0300

    requisito 2, lint ok

--- src/components/LoginBox.jsx
+++ src/components/LoginBox.jsx
@@ -1,64 +1,75 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class LoginBox extends Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
       emailValid: false,
     };
   }
-  
+
   componentDidUpdate(prevProps) {
-    this.emailValidation(prevProps)
+    this.emailValidation(prevProps);
   }
 
-  emailValidate(email) {
-    const email_validate = /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/;
-    return email_validate.test(email);
+  emailValidate(email, password) {
+    const emailValidate = /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/;
+    const passwordMinLength = 6;
+    const validPassword = password.length >= passwordMinLength;
+    const validEmail = emailValidate.test(email);
+    return validPassword && validEmail;
   }
 
   emailValidation({ password: prevP, email: prevE }) {
-    const { password, email } = this.props;
-    if(prevE !== email || prevP !== password) {
-      if (this.emailValidate(email) && password.length >= 6) {
+    const { password, email, emailValid } = this.props;
+    if (prevE !== email || prevP !== password) {
+      if (this.emailValidate(email, password)) {
         this.setState({ emailValid: true });
-      }
-      else if (!(this.props.emailValid)) {
+      } else if (!(emailValid)) {
         this.setState({ emailValid: false });
       }
     }
   }
 
-  render () {
+  render() {
     const { getInputs, onSubmitBtn } = this.props;
     const { emailValid } = this.state;
     return (
       <div>
-      <h3>Login</h3>
-      <div>
-        <label>
-          Email:
-          <input
-            data-testid="email-input"
-            type="email"
-            onChange={getInputs}
-          />
-        </label>
-        <label>
-          Senha:
-          <input
-            data-testid="password-input"
-            type="password"
-            onChange={getInputs}
-          />
-        </label>
-        {
-          emailValid
-          ? <button onClick={onSubmitBtn} >Entrar</button>
-          : <button disabled >Entrar</button>
-        }
+        <h3>Login</h3>
+        <div>
+          <label htmlFor="email-input">
+            Email:
+            <input
+              data-testid="email-input"
+              type="email"
+              onChange={ getInputs }
+            />
+          </label>
+          <label htmlFor="password-input">
+            Senha:
+            <input
+              data-testid="password-input"
+              type="password"
+              onChange={ getInputs }
+            />
+          </label>
+          {
+            emailValid
+              ? <button type="submit" onClick={ onSubmitBtn }>Entrar</button>
+              : <button type="submit" disabled>Entrar</button>
+          }
+        </div>
       </div>
-    </div>
-    )
+    );
   }
 }
+
+LoginBox.propTypes = {
+  password: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  emailValid: PropTypes.bool.isRequired,
+  getInputs: PropTypes.func.isRequired,
+  onSubmitBtn: PropTypes.func.isRequired,
+};
