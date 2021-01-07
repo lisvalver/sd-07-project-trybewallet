d14038dfdc8fcf38db9e592d07c5eeb516fc239c
Felipe Nascimento <flpnascto@gmail.com>
2021-01-07 17:40:20 -0300

    ExpenseForm para Wallet

---
+++ src/components/ExpenseForm.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ExpenseForm extends Component {
+  render() {
+    return (
+      <div>
+        Formulário de Despesas
+      </div>
+    );
+  }
+}
+
+export default ExpenseForm;
