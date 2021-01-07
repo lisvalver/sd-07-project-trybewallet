c96871da1b2ef7baf569cd41cbc72f71c9dc5475
Lorena Góes <goesloren@gmail.com>
2021-01-07 19:15:12 -0300

    Adicionando rota para Wallet

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Login } from '../pages';
+import { Login, Wallet } from '../pages';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={ Login } />
+    <Route path="/carteira" component={ Wallet } />
   </Switch>
 );
 
