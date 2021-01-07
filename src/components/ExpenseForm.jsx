065457fdfde1724688289d9dc5858d9a0568b5b3
Felipe Nascimento <flpnascto@gmail.com>
2021-01-07 17:46:21 -0300

    Definir state de ExpenseForm

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 
 class ExpenseForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: 0,
+      description: '',
+      currency: 'BRL',
+      paymentMethod: 'Dinheiro',
+      categoty: 'Alimentação',
+    };
+  }
+
   render() {
     return (
       <div>
