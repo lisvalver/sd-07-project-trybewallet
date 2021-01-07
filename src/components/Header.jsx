2cc1a56bdec69b5a4842f55bb58996af5a575c57
Felipe Nascimento <flpnascto@gmail.com>
2021-01-07 17:21:19 -0300

    Inserir mapStateToProps no Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const despesaTotal = 0;
+    const { loggedEmail } = this.props;
     return (
       <div>
         <h1>Carteira</h1>
-        <span>
-          {`E-mail:`}
+        <span data-testid="email-field">
+          {`E-mail: ${loggedEmail}`}
+        </span>
+        <span data-testid="total-field">
+          {`Despesa Total: ${despesaTotal}`}
+        </span>
+        <span data-testid="header-currency-field">
+          {` BRL`}
         </span>
       </div>
     );
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  const { user } = state;
+  return { loggedEmail: user.email };
+}
+
+Header.propTypes = {
+  loggedEmail: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
