573d85e2eb528b282fe5163d5ffdaa71d7acc0c5
brendalima <brenda.c.lima@poli.ufrj.br>
2021-01-08 14:53:39 -0300

    adding forms and button for login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+    <div>
+      <form>
+        <input trype="text" data-testid="email-input" placeholder="E-mail"></input>
+        <input trype="password" data-testid="password-input" placeholder="Senha"></input>
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+    );
   }
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import user from './user';
 import wallet from './wallet';
 import { combineReducers } from 'redux';
 
-combineReducers( {
+export default combineReducers( {
   user,
   wallet,
 })
