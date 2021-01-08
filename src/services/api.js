b4f2b65380752bb525c2bb308c89ef5173fa38a3
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-01-08 13:38:56 -0300

    requisito 4

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
-const getCurrencies = () =>  new Promise((resolve, reject) => {
+const getCurrencies = () => new Promise((resolve, reject) => {
   const api = 'https://economia.awesomeapi.com.br/json/all';
   fetch(api).then((response) => {
     response.json()
-      .then((data) => resolve(data))
+      .then((data) => resolve(data));
   }).catch((error) => reject(error));
 });
 
