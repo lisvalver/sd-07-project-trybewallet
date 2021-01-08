3d785d49de9d700d1a1a0cbd7c0ad2b26cd8b115
Emerson Moonlit <emoonlit@pop-os.localdomain>
2021-01-08 15:39:40 -0300

    Login Page Basic

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,36 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form className="login">
+          <label htmlFor="email">
+            Login
+            <input
+              id="email"
+              data-testid="email-input"
+              type="text"
+              name="email"
+            />
+          </label>
+          <label htmlFor="password">
+            Password
+            <input
+              id="password"
+              type="password"
+              data-testid="password-input"
+              name="password"
+            />
+          </label>
+          <button
+            className="button-entrar"
+            type="button"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
