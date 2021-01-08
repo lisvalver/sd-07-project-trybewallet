08f63265901c7fabc5fb527811806d3312116c4e
MoisesSantana <moisaant@gmail.com>
2021-01-08 10:05:44 -0300

    correcoes lint

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
-export async function callAPI() {
+export default async function callAPI() {
   const url = 'https://economia.awesomeapi.com.br/json/all';
   let apiResult = await fetch(url);
   apiResult = await apiResult.json();
-  return apiResult; 
+  return apiResult;
 }
