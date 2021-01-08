ab20ac430ad27e249328a6fe19fc27cebbd32a30
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2021-01-08 16:51:04 -0300

    requi 1 a 3

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -4,10 +4,8 @@ const INITIAL_STATE = {
 
 };
 function userReducer(state = INITIAL_STATE, action) {
-  console.log('oi', action.type);
   switch (action.type) {
   case 'LOGIN': {
-    console.log(state);
     return { email: action.email, logged: true };
   }
 
