2b61471c86c97253a5b4eb7cff59b84ecff21f0f
Lorena Góes <goesloren@gmail.com>
2021-01-08 14:02:58 -0300

    Corrigindo erros do avaliador

--- src/store/ducks/user/index.js
+++ src/store/ducks/user/index.js
@@ -1,6 +1,9 @@
 import userTypes from './types';
+import initialState from '../initialState';
 
-const user = (state = {}, action) => {
+const INITIAL_STATE = initialState.user;
+
+const user = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case userTypes.LOGIN:
     return { ...state, email: action.email };
---
+++ src/store/ducks/wallet/index.js
@@ -0,0 +1,12 @@
+import initialState from '../initialState';
+
+const INITIAL_STATE = initialState.wallet
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default wallet;
