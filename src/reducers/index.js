7f0c99f8477a7e767bd21e060fc12f9baf641f0c
brendalima <brenda.c.lima@poli.ufrj.br>
2021-01-07 23:39:45 -0300

    configurando redux ate o rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,11 @@
-// import user from './user';
-// import wallet from './wallet';
+import user from './user';
+import wallet from './wallet';
+import { combineReducers } from 'redux';
+
+combineReducers( {
+  user,
+  wallet,
+})
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
