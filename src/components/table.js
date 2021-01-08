fd8ae1165a5d58f17dd19bef008255466434b89d
Fernando Antunes <fernando.antunes1@gmail.com>
2021-01-08 16:36:04 -0300

    Finalizado requisito 5

--- src/components/table.js
+++ src/components/table.js
@@ -1,11 +1,65 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Table extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.renderTableData = this.renderTableData.bind(this);
+    this.renderTableHeader = this.renderTableHeader.bind(this);
+  }
+
+  // Nestas funções eu usei como base este site: https://dev.to/abdulbasit313/an-easy-way-to-create-a-customize-dynamic-table-in-react-js-3igg
+  renderTableData() {
+    const { expenses } = this.props;
+    return expenses.map((data, index) => {
+      const { description, tag, method, value, currency, exchangeRates } = data;
+      const {
+        [currency]: { ask, name },
+      } = exchangeRates;
+      return (
+        <tr key={ index }>
+          <td>{description}</td>
+          <td>{tag}</td>
+          <td>{method}</td>
+          <td>{value}</td>
+          <td>{name}</td>
+          <td>{(Math.round(ask * 100) / 100).toFixed(2)}</td>
+          <td>{(Math.round(ask * value * 100) / 100).toFixed(2)}</td>
+          <td>Real</td>
+          <td />
+        </tr>
+      );
+    });
+  }
+
+  renderTableHeader() {
+    const header = [
+      'Descrição',
+      'Tag',
+      'Método de pagamento',
+      'Valor',
+      'Moeda',
+      'Câmbio utilizado',
+      'Valor convertido',
+      'Moeda de conversão',
+      'Editar/Excluir',
+    ];
+    return header.map((key, index) => <th key={ index }>{key}</th>);
+  }
+
   render() {
     return (
-      <div>Tabela</div>
+      <div>
+        <h3>Tabela de despesas</h3>
+        <table>
+          <tbody>
+            <tr>{this.renderTableHeader()}</tr>
+            {this.renderTableData()}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
@@ -16,11 +70,6 @@ const mapStateToProps = (state) => ({
 
 export default connect(mapStateToProps)(Table);
 
-// Descrição -> Vem do state global (expenses[0].description)
-// Tag -> Vem do state global (expenses[0].tag)
-// Método de pagamento -> Vem do state global (expenses[0].method)
-// Valor -> Vem do state global (expenses[0].value)
-// Moeda -> Vem do state global (expenses[0].currency)
-// Câmbio utilizado -> Vem do state global (expenses[0].exchangeRates(find).ask)
-// Valor convertido -> Cambio utilizado * Valor
-// Moeda de conversão -> 'BRL' (a confirmar)
+Table.propTypes = {
+  expenses: PropTypes.array,
+}.isRequired;
