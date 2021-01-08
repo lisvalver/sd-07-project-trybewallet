18140d59c47da6dde7b009f9b8a2b72fae0dda96
luc-zago <luc.zago@hotmail.com>
2021-01-08 15:34:18 -0300

    Finalizado projeto

--- src/components/CurrencyOption.jsx
+++ src/components/CurrencyOption.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
 
 class CurrencyOption extends Component {
   render() {
     const { currency } = this.props;
-      return (<option value={ currency } data-testid={ currency }>{ currency }</option>);
+    return (<option value={ currency } data-testid={ currency }>{ currency }</option>);
   }
 }
 
 export default CurrencyOption;
+
+CurrencyOption.propTypes = {
+  currency: propTypes.string,
+}.isRequired;
