03893c94cceada9bede3d28de017307bab64a8a1
Strongreen <strongreen@live.com>
2021-01-08 14:31:07 -0300

    correçoes lint

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import './Login.css';
 import signIn from '../actions';
