72bc4750addc3aa6f74a856050ae3c69b9a1cf6b
brendalima <brenda.c.lima@poli.ufrj.br>
2021-01-08 19:57:01 -0300

    adding wallet page header

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <div>
+        <header>
+          <span data-testid="email-field">{ email }</span>
+          <span data-testid="total-field">Depesa geral: 0</span>
+          <span data-testid="header-currency-field">Câmbio: BRL</span>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
