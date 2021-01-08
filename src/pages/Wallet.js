b01edaa1c33100ac5bae6e6d2b72dee50b73763c
brendalima <brenda.c.lima@poli.ufrj.br>
2021-01-08 20:31:17 -0300

    adding inputs for wallet page and api request

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,6 +3,21 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.apiFetch = this.apiFetch.bind(this);
+  }
+
+  async apiFetch() {
+    try {
+      const requestCurrencies = await fetch('https://economia.awesomeapi.com.br/json/all');
+      const result = requestCurrencies.json();
+      return result;
+    } catch (error) {
+      return error;
+    }
+  }
+
   render() {
     const { email } = this.props;
     return (
@@ -12,6 +27,13 @@ class Wallet extends React.Component {
           <span data-testid="total-field">Depesa geral: 0</span>
           <span data-testid="header-currency-field">Câmbio: BRL</span>
         </header>
+        <form>
+          <input type="text" data-testid="value-input" placeholder="Valor"/>
+          <input type="text" data-testid="description-input" placeholder="Descrição"/>
+          <select>
+            <option></option>
+          </select>
+        </form>
       </div>
     );
   }
