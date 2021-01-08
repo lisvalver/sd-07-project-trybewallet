18aee62a9e4322c13bd6c9b0f3c495c27318db01
brendalima <brenda.c.lima@poli.ufrj.br>
2021-01-08 18:47:50 -0300

    adding email and password validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,15 +2,37 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions'
 
+const MIN_PASSWORD_LENGTH = 6;
+
+function emailIsValid (email) {
+  return /\S+@\S+\.\S+/.test(email);
+}
+
+function passwordIsValid (password) {
+  return password.length >= MIN_PASSWORD_LENGTH;
+}
+
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.handleEvent = this.handleEvent.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.inputValidation = this.inputValidation.bind(this);
     this.state = {
       email: '',
       password: '',
     };
+  }
+
+  inputValidation() {
+    const { email, password } = this.state;
+    const emailValidation = emailIsValid(email);
+    const passwordValidation = passwordIsValid(password);
+
+    if (emailValidation && passwordValidation) {
+      return false
+    }
+    return true;
   }
 
   handleEvent({target: {name, value}}) {
@@ -42,20 +64,15 @@ class Login extends React.Component {
         name="password"
         onChange={this.handleEvent}
         value={password}></input>
-        <button type="button" onClick={this.handleClick}>Entrar</button>
+        <button disabled={this.inputValidation()} type="button" onClick={this.handleClick}>Entrar</button>
       </form>
     </div>
     );
   }
 }
 
-// const mapStateToProps = (state) => ({
-//   email: state.user.email,
-// });
-
 const mapDispatchToProps = {
   saveEmail: actions.saveEmail,
 }
-
 
 export default connect(null, mapDispatchToProps)(Login);
