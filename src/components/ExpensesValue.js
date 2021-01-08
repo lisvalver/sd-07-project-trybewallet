c4f3cad7bdde46a433f2f5d6043f24740d4a06ac
Jr3564 <rodolfojr94@gmail.com>
2021-01-08 13:41:08 -0300

    requisito 3

--- src/components/ExpensesValue.js
+++ src/components/ExpensesValue.js
@@ -3,26 +3,10 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class ExpensesValue extends Component {
-  constructor() {
-    super();
-    this.state = {
-      expenses: 0,
-    };
-  }
-
-  componentDidMount() {
-    this.updateExpenses();
-  }
-
-  updateExpenses() {
-    const { expenses } = this.props;
-    this.setState({ expenses });
-  }
-
   render() {
-    const { expenses } = this.state;
+    const { expenses } = this.props;
     return (
-      <spam data-testid="total-field">{expenses}</spam>
+      <spam data-testid="total-field">{+expenses}</spam>
     );
   }
 }
