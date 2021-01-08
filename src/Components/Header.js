37af6e9c4d9ace788368133c6fe31b70e7debf0e
Strongreen <strongreen@live.com>
2021-01-08 18:35:47 -0300

    Aeee agora foi requisito 3

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,16 +4,24 @@ import { connect } from 'react-redux';
 import { wallet } from '../actions';
 
 class Header extends Component {
+  componentDidMount() {
+    const { despesasTotais } = this.props;
+    const despesa = 0;
+    despesasTotais(despesa);
+  }
+
   render() {
     const { email, despesas } = this.props;
     return (
       <header>
         <section data-testid="email-field">
           Email:
+          { ' ' }
           { email }
         </section>
         <section data-testid="total-field">
           Despesas Totais: R$
+          { ' ' }
           { despesas }
         </section>
         <section data-testid="header-currency-field">BRL</section>
@@ -25,14 +33,16 @@ class Header extends Component {
 Header.propTypes = {
   email: PropTypes.string.isRequired,
   despesas: PropTypes.string.isRequired,
+  despesasTotais: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
+  despesas: state.wallet.despesas,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  despesasTotais: (payload) => dispatch(wallet(payload)),
+  despesasTotais: (despesaTotal) => dispatch(wallet(despesaTotal)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,10 +1,14 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
-const INITIAL_STATE = 0;
+const INITIAL_STATE = [
+  {
+    despesas: 0,
+  },
+];
 
 function wallet(state = INITIAL_STATE, action) {
   switch (action.type) {
   case 'DESPESAS':
-    return [...state, action.despesas];
+    return { despesas: action.despesas };
   default:
     return state;
   }
