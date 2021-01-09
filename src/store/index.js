427ff66cfcf9410984237224e2fcee9c7c9561b4
spawnred <bruno.spawnhell@gmail.com>
2021-01-09 00:23:13 -0300

    item 1 e 2

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import rootReducer from '../reducers/index';
 
 const store = createStore(
   rootReducer, /* preloadedState, */
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
 export default store;
