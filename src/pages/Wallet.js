68fdf4e5d6898c956245bc5d6c9219f5bfeeeca9
Bruno Nogueira <nugobruno@gmail.com>
2021-01-09 04:10:09 -0300

    Eslint issues IV

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 class Wallet extends React.Component {
   render() {
-    return(
+    return (
       <header>
         <h2 data-testid="email-field">a</h2>
       </header>
