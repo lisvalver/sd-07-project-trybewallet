9a4928cce2eb70e890577d4e555b181e818d7beb
Strongreen <strongreen@live.com>
2021-01-09 13:18:27 -0300

    criando o fetch

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,3 @@
 export { default as signIn } from './signIn';
 export { default as wallet } from './wallet';
+export { default as getApi } from './getApi';
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import reducerAll from '../reducers';
 
-const store = createStore(
-  reducerAll,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducerAll, composeEnhancer(applyMiddleware(thunk)));
 
 export default store;
