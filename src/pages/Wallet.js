2e33ea57993db4183b87dae13e3eae1631834e08
murilopereira006 <muriloedu006@gmail.com>
2021-01-10 00:45:04 -0300

    que diaxo de erro eh esse

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { fetchMoedaAPI } from '../actions';
 
 class Wallet extends React.Component {
   constructor() {
@@ -8,11 +9,28 @@ class Wallet extends React.Component {
     this.state = {
       atualExpenses: 0,
     };
+
+    this.currenciesOption = this.currenciesOption.bind(this);
+  }
+
+  componentDidMount() {
+    const { getAPI } = this.props;
+    getAPI();
+  }
+
+  currenciesOption() {
+    const { currencies } = this.props;
+    const choosedCoin = Object.entries(currencies);
+    const filtered = choosedCoin.filter(
+      (currencie) => currencie[0] !== 'USDT',
+    );
+    return filtered;
   }
 
   render() {
     const { email, currencies } = this.props;
     const { atualExpenses } = this.state;
+    const currenciesOption = this.currenciesOption();
 
     return (
       <div>
@@ -28,8 +46,19 @@ class Wallet extends React.Component {
           </label>
           <label htmlFor="discription">
             Descrição da despesa
-            <input name="discription" type="number" data-testid="description-input" />
+            <input name="discription" type="text" data-testid="description-input" />
           </label>
+          <select>
+            {currenciesOption.map((currencie) => (
+              <option
+                key={ currencie[0] }
+                value={ currencie[0] }
+                data-testid={ currencie[0] }
+              >
+                {currencie[0]}
+              </option>
+            ))}
+          </select>
         </form>
       </div>
     );
@@ -39,12 +68,18 @@ class Wallet extends React.Component {
 Wallet.propTypes = {
   email: PropTypes.string.isRequired,
   currencies: PropTypes.arrayOf.isRequired,
+  isFetching: PropTypes.bool.isRequired,
 };
+
+const mapDispatchToProps = (dispatch) => ({
+  getAPI: () => dispatch(fetchMoedaAPI()),
+});
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
   expenses: state.wallet.expenses,
   currencies: state.wallet.currencies,
+  isFetching: state.wallet.isFetching,
 });
 
-export default connect(mapStateToProps)(Wallet);
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
