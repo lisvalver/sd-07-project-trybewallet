7d150144096732d238b1c291089ba7a7912b9beb
Felipe Nascimento <flpnascto@gmail.com>
2021-01-10 14:24:19 -0300

    Actions para API e adicionar despesa

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,42 @@
-// Coloque aqui suas actions
-const signIn = (payload) => ({
-  type: 'LOGIN',
+import * as currencyAPI from '../services/api';
+
+const LOGIN = 'LOGIN';
+const ADD_EXPENSE = 'ADD_EXPENSE';
+const REQUEST_CURRENCY = 'GET_CURRENCY';
+const REQUEST_CURRENCY_FAIL = 'REQUEST_CURRENCY_FAIL';
+
+export const signIn = (payload) => ({
+  type: LOGIN,
   payload,
 });
 
-export default signIn;
+function requestCurrency() {
+  return ({
+    type: REQUEST_CURRENCY,
+  });
+}
+
+function requestCurrencyFail(error) {
+  return ({
+    type: REQUEST_CURRENCY_FAIL,
+    payload: error,
+  });
+}
+
+export function addExpense(expenseDetails, exchangeRates) {
+  return ({
+    type: ADD_EXPENSE,
+    payload: Object.assign(expenseDetails, { exchangeRates }),
+  });
+}
+
+export function fetchCurrencyAPI(expenseDetails) {
+  return (dispatch) => {
+    dispatch(requestCurrency());
+    return currencyAPI.getValues()
+      .then(
+        (exchangeRates) => dispatch(addExpense(expenseDetails, exchangeRates)),
+        (error) => dispatch(requestCurrencyFail(error)),
+      );
+  };
+}
