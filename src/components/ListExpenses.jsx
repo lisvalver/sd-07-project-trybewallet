e5f5395ba6f0bf0e06e9a38aa4de7cca3af36542
Felipe Nascimento <flpnascto@gmail.com>
2021-01-10 15:45:13 -0300

    Componente para Tebela de Despesas

---
+++ src/components/ListExpenses.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ListExpenses extends Component {
+  render() {
+    return (
+      <div>
+        Tabela de Despesas
+      </div>
+    );
+  }
+}
+
+export default ListExpenses;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import ExpenseForm from '../components/ExpenseForm';
+import ListExpenses from '../components/ListExpenses';
 
 class Wallet extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class Wallet extends React.Component {
       <div>
         <Header />
         <ExpenseForm />
+        <ListExpenses />
       </div>
     );
   }
