d1e97f9d8247c98b2086a8014abc474f9ef6dd44
vitorAmorims <vtamorims@gmail.com>
2021-01-10 17:13:58 -0300

    correcoes de sintaxe

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import { connect } from 'react-redux'
-import store from '../store/index'
+import { connect } from 'react-redux';
+// import store from '../store/index'
 
 const Header = (props) => {
   const { totalExpense } = props;
   // console.log(props.state.wallet.expenses);
   // const expenses = props.state.wallet;
   // console.log(expenses);
-    
-  const { state: { user } } = props;
+  const { state: { user: { email } } } = props;
   const dez = 10;
-  
+
   return (
     <div>
       <span data-testid="email.field" htmlFor="email">
-        Email: <span data-testid="email-field">{Object.values(user.email)}</span>
+        Email: <span data-testid="email-field">{Object.values(email)}</span>
       </span>
-      <span data-testid="total-field">Despesa total: {parseFloat(totalExpense, dez).toFixed(2)} </span>
+      <span data-testid="total-field">
+        Despesa total: {parseFloat(totalExpense, dez).toFixed(2)}
+      </span>
       <span data-testid="header-currency-field">BRL</span>
     </div>
   )
@@ -25,8 +26,8 @@ const Header = (props) => {
 function mapStateToProps(state) {
   // console.log(state);
   return {
-    state
-  }
+    state,
+  };
 }
 
 export default connect(mapStateToProps)(Header);
