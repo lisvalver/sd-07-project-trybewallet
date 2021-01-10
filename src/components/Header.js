b84b1ddc4f6ae1255640ebd57f1dc20c51f0bc83
Samanta Below <samanta.below@gmail.com>
2021-01-09 21:33:55 -0300

    Requisito 4 finalizado

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,23 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.sumExpenses = this.sumExpenses.bind(this);
+  }
+
+  sumExpenses(expenses) {
+    return expenses.reduce((accum, currentValue) => accum
+      + parseFloat(currentValue.value)
+      * (parseFloat(currentValue.exchangeRates[currentValue.currency].ask)), 0);
+  }
+
   render() {
-    const { userEmail } = this.props;
+    const { userEmail, expenses } = this.props;
     return (
       <div>
         <div data-testid="email-field">{ userEmail }</div>
-        <div data-testid="total-field">0</div>
+        <div data-testid="total-field">{ this.sumExpenses(expenses) }</div>
         <div data-testid="header-currency-field">BRL</div>
       </div>
     );
@@ -17,10 +28,12 @@ class Header extends React.Component {
 
 const mapStateToProps = (state) => ({
   userEmail: state.user.email,
+  expenses: state.wallet.expenses,
 });
 
 export default connect(mapStateToProps)(Header);
 
 Header.propTypes = {
   userEmail: PropTypes.string.isRequired,
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
