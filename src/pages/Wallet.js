b6bb2255123368e75337fd0c047013e8aa6e2ed6
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2021-01-10 16:59:33 -0300

    Alteraçoes para o terceiro requisito

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expense: 0,
+      currency: 'BRL',
+    };
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { emailState } = this.props;
+    const { expense, currency } = this.state;
+    return (
+      <div>
+        <header>
+          <h1>TrybeWallet</h1>
+          <p data-testid="email-field">{ emailState }</p>
+          <p data-testid="total-field">{ expense }</p>
+          <p data-testid="header-currency-field">{ currency }</p>
+        </header>
+      </div>);
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  emailState: state.user.email,
+});
+
+Wallet.propTypes = {
+  emailState: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
