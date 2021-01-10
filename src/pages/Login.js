1e91b0f86019fefd0693a3e36fb096f3f97befab
Lucas <lafdesouza2002@outlook.com>
2021-01-10 01:59:41 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-import { addUser } from '../actions';
+import { addUser } from '../actions/index';
 
 class Login extends React.Component {
   constructor() {
