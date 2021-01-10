5d291b456622f7af47e86e9f35c0af4e378b8d39
Felipe Nascimento <flpnascto@gmail.com>
2021-01-10 14:22:41 -0300

    Incrementando serviços da  API

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,13 @@
 export async function getTypes() {
-  const response = await fetch('https://economia.awesomeapi.com.br/json/all');
-  return response.json();
+  const request = await fetch('https://economia.awesomeapi.com.br/json/all');
+  const response = await request.json();
+  const currencyTypes = Object.values(response)
+    .filter((value) => value.codein === 'BRL')
+    .map((value) => value.code);
+  return currencyTypes;
 }
 
-export async function getValue() {
+export async function getValues() {
   const response = await fetch('https://economia.awesomeapi.com.br/json/all');
   return response.json();
 }
