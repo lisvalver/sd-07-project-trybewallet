d2ca4466fcc76b363fdb10ed1caf8916471d8d49
Felipe Nascimento <flpnascto@gmail.com>
2021-01-10 16:12:36 -0300

    Conectando ListExpenses à store

--- src/components/ListExpenses.jsx
+++ src/components/ListExpenses.jsx
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class ListExpenses extends Component {
   render() {
+    const { expenses } = this.props;
+    console.log(expenses);
     return (
       <div>
-        Tabela de Despesas
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>January</td>
+              <td>$100</td>
+            </tr>
+            <tr>
+              <td>February</td>
+              <td>$80</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td>Sum</td>
+              <td>$180</td>
+            </tr>
+          </tfoot>
+        </table>
       </div>
     );
   }
 }
 
-export default ListExpenses;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+ListExpenses.propTypes = {
+  expenses: PropTypes.arrayOf(Object).isRequired,
+};
+
+export default connect(mapStateToProps)(ListExpenses);
