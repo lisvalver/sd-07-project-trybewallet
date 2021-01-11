c11e6038d24cca4024e8973eb2b2a3b33b1dc9c9
moraisgabri <gabriel8azevedo@gmail.com>
2021-01-11 14:10:46 -0300

    requisito 4

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,3 +3,15 @@ export const loginAction = (email) => ({
   type: 'LOGIN',
   email,
 });
+
+export const currencyAction = (currency) => ({
+  type: 'CURRENCY',
+  currency,
+});
+
+export const fetchingAPI = () => async (dispatch) => {
+  const endpoint = 'https://economia.awesomeapi.com.br/json/all';
+  const response = await (await (await fetch(endpoint)).json());
+  dispatch(currencyAction(response));
+  return response;
+};
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,15 +1,67 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import * as action from '../actions';
 
 class Wallet extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleFetch = this.handleFetch.bind(this);
+  }
+
+  componentDidMount() {
+    this.handleFetch();
+  }
+
+  handleFetch() {
+    const { fetchMyCurrencyList } = this.props;
+    fetchMyCurrencyList();
+  }
+
   render() {
-    const { userLogin } = this.props;
+    const { userLogin, currencyList } = this.props;
+    const filterCurrencies = Object.keys(currencyList)
+      .filter((currency) => currency !== 'USDT');
+    console.log(currencyList);
     return (
       <div>
-        <span data-testid="email-field">{ userLogin }</span>
-        <span data-testid="total-field">0</span>
-        <span data-testid="header-currency-field">BRL</span>
+        <div>
+          <span data-testid="email-field">{userLogin}</span>
+          <span data-testid="total-field">0</span>
+          <span data-testid="header-currency-field">BRL</span>
+        </div>
+        <div>
+          <form>
+            <label htmlFor="expense">
+              Valor da despeza:
+              <input type="text" id="expense" data-testid="value-input" />
+            </label>
+            <label htmlFor="description">
+              Descrição da despeza:
+              <input type="text" id="description" data-testid="description-input" />
+            </label>
+            <label htmlFor="currency">
+              Selecione o Câmbio
+              <select data-testid="currency-input" id="currency">
+                {Object.keys(filterCurrencies).map((currency, index) => {
+                  if (currency !== 'USDT') {
+                    return (
+                      <option
+                        data-testid={ currency }
+                        key={ index }
+                        value={ currency }
+                      >
+                        {currency}
+                      </option>
+                    );
+                  }
+                  return null;
+                })}
+              </select>
+            </label>
+          </form>
+        </div>
       </div>
     );
   }
@@ -17,10 +69,17 @@ class Wallet extends Component {
 
 const mapStateToProps = (state) => ({
   userLogin: state.user.email,
+  currencyList: state.wallet.currencies,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchMyCurrencyList: () => dispatch(action.fetchingAPI),
 });
 
 Wallet.propTypes = {
   userLogin: PropTypes.string.isRequired,
+  fetchMyCurrencyList: PropTypes.func.isRequired,
+  currencyList: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
-export default connect(mapStateToProps, null)(Wallet);
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
