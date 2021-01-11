5994c5805f1cfbbde5c3b8463dcd401db495718d
IsabelaCardoso <pcardoso.isabela@gmail.com>
2021-01-10 21:24:05 -0300

    Requirement 1, 2 and 3

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,6 @@
-// Coloque aqui suas actions
+export const EMAIL = 'EMAIL';
+
+export const login = (email) => ({
+  type: EMAIL,
+  email,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,75 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { login } from '../actions/index';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.emailHandler = this.emailHandler.bind(this);
+    this.passwordHandler = this.passwordHandler.bind(this);
+    this.dataVerify = this.dataVerify.bind(this);
+    this.submitBtn = this.submitBtn.bind(this);
+  }
+
+  // https://www.w3resource.com/javascript/form/email-validation.php
+
+  dataVerify() {
+    const { email, password } = this.state;
+    const minLength = 6;
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+      .test(email) && password.length >= minLength) {
+      return true;
+    }
+    return false;
+  }
+
+  emailHandler(event) {
+    const { value } = event.target;
+    this.setState({ email: value });
+  }
+
+  passwordHandler(event) {
+    const { value } = event.target;
+    this.setState({ password: value });
+  }
+
+  submitBtn() {
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email } = this.state;
+    const { addEmail } = this.props;
+    return (
+      <div>
+        <input onChange={ this.emailHandler } type="email" data-testid="email-input" />
+        <input
+          onChange={ this.passwordHandler }
+          type="password"
+          data-testid="password-input"
+        />
+        {/* Código do Luciano Berchon: botão dentro do <Link> */}
+        <Link to="/carteira">
+          <button
+            onClick={ () => addEmail(email) }
+            type="button"
+            disabled={ !this.dataVerify() }
+          >
+            Entrar
+          </button>
+        </Link>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  addEmail: (email) => dispatch(login(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
