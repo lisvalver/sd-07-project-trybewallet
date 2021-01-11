2b22b9df2f9eb96413ed19c224e7de79c2570b8f
marcos <marcos.1998226@gmail.com>
2021-01-11 19:05:54 -0300

    requisito 4

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
 import { applyMiddleware, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import reducer from '../reducers';
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+// const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+// export default store;
+const store = createStore(reducer, applyMiddleware(thunk));
 export default store;
