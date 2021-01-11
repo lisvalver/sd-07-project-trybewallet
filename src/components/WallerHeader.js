ef8732cc186f51152bf7bce50c09cf57faeb3302
IsabelaCardoso <pcardoso.isabela@gmail.com>
2021-01-10 21:24:53 -0300

    starting requirement 4

---
+++ src/components/WallerHeader.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class WalletHeader extends React.Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <header>
+        <span data-testid="email-field">{ email }</span>
+        <span data-testid="total-field">0</span>
+        <span data-testid="header-currency-field">BRL</span>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(WalletHeader);
