3a47f50b0d1cd7d8df60c212b70a89f6e49963f0
IzeldaValeriano <zel.valeriano@gmail.com>
2021-01-11 09:49:13 -0300

    incio de projeto, rederização da pagina login concluida

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Login from './pages/Login.js';
+import { Switch, Route } from 'react-router-dom';
+
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return <div>
+   <Switch>
+     <Route component={ Login } exact path='/'/>
+   </Switch>
+    </div>;
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,36 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      validEmail: false,
+      validpassword: false,
+    };
+  }
+
+  /*  validatePasswords(password) => {
+    return password.length >= 6;
+  };
+
+  validateEmail(email) => {
+    const pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    return RegExp(pattern).test(email)
+  } */
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <label htmlFor="email">
+          <input data-testid="email-input" id="email" type="email" id="email" />
+        </label>
+        <label htmlFor="password">
+          <input data-testid="password-input" id="password" type="password" id="password" />
+        </label>
+        <button>Entrar</button>
+      </form>
+    );
   }
 }
 
