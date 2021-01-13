8c532352f3627f2a717c2cff26db37875ea79b49
Filipi Firmino <contatoic3web@gmail.com>
2021-01-13 12:55:30 -0300

    Fix error 3

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes  from 'prop-types'
+import PropTypes from 'prop-types';
 import Logo from '../images/logo-wallet.jpg';
 
 class Header extends React.Component {
