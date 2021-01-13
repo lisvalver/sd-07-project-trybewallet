5341881fb2630a9c91bb4bbf6a885e17b052d6e9
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-01-13 15:21:25 -0300

    Commit: 004

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
-import Wallet from './pages/Wallet';
 import store from './store';
 
 ReactDOM.render(
-  <Provider store={ store }>
-    <BrowserRouter>
-      <Route exact path="/" component={ App } />
-      <Route exact path="/carteira" component={ Wallet } />
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={ store }>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
