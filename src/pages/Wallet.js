65a9feeaf07143821839542196340990ce1adcc4
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-01-13 16:12:26 -0300

    Commit: 005

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      total: 0,
+      cambio: 'BRL',
+    };
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { total, cambio } = this.state;
+    const { email } = this.props;
+    return (
+      <div>
+        TrybeWallet
+        <header data-testid="email-field">
+          {email}
+        </header>
+
+        <p data-testid="total-field">
+          {`Despesas: ${total}`}
+        </p>
+
+        <p data-testid="header-currency-field">
+          {`Cambio: ${cambio}`}
+        </p>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
