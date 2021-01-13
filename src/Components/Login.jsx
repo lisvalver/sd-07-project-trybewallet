28c2edcc9125274fffdf21ae73b0c7ab5cfc7870
Filipi Firmino <contatoic3web@gmail.com>
2021-01-13 13:50:53 -0300

    order import

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { login } from '../actions/user.action';
-import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
   constructor(props) {
