6ca4a9cfc9394603787720789041f955a9987d74
Camila Valentim <camilaifpb@hotmail.com>
2021-01-15 09:34:39 -0300

    requisito 05

--- src/componentes/Tabela.js
+++ src/componentes/Tabela.js
@@ -1,23 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-class Tabela extends React.Component {
+class Tabela extends Component {
+  convertValues(value, currency) {
+    const valor = Math.round(value * currency * 100) / 100;
+    return valor;
+  }
+
+  // Referencia Alexandre Faustino
+
   render() {
+    const { expense } = this.props;
     return (
       <div>
-        <table border="1" width="400">
-          <tr>
-            <th>descricao</th>
-            <th>tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-          </tr>
-        </table>
+        <section>
+          <table>
+            <thead>
+              <tr>
+                <th>Descrição</th>
+                <th>Tag</th>
+                <th>Método de pagamento</th>
+                <th>Valor</th>
+                <th>Moeda</th>
+                <th>Câmbio utilizado</th>
+                <th>Valor convertido</th>
+                <th>Moeda de conversão</th>
+                <th>Editar/Excluir</th>
+              </tr>
+            </thead>
+            <tbody>
+              {expense.map((item) => {
+                const {
+                  id,
+                  description,
+                  tag,
+                  method,
+                  value,
+                  currency,
+                  exchangeRates,
+                } = item;
+                const {
+                  [currency]: { name, ask },
+                } = exchangeRates;
+                return (
+                  <tr key={ id }>
+                    <td>{description}</td>
+                    <td>{tag}</td>
+                    <td>{method}</td>
+                    <td>{value}</td>
+                    <td>{name}</td>
+                    <td>{parseFloat(ask).toFixed(2)}</td>
+                    <td>{this.convertValues(value, ask)}</td>
+                    <td>Real</td>
+                    <td>
+                      <button
+                        type="button"
+                        data-testid="edit-btn"
+                      >
+                        Editar
+                      </button>
+                      <button
+                        type="button"
+                        data-testid="delete-btn"
+                      >
+                        Excluir
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </section>
       </div>
     );
   }
 }
-export default Tabela;
+const mapStateToProps = (state) => ({
+  expense: state.wallet.expenses,
+});
+Tabela.propTypes = {
+  expense: PropTypes.arrayOf(Object).isRequired,
+};
+export default connect(mapStateToProps, null)(Tabela);
