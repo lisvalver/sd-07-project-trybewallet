4ff4e3c74c79207e3f0b2a281d9eb9939a8c54ce
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-01-15 00:07:05 -0300

    Começando requisito 5

---
+++ src/components/Table.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+
+class Table extends Component {
+  render() {
+    const { expenses } = this.props;
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense, index) => {
+            const {
+              description,
+              tag,
+              method,
+              value,
+              currency,
+              exchangeRates,
+            } = expense;
+            const rate = parseFloat(exchangeRates[currency].ask).toFixed(2);
+            const converted = (rate * value).toFixed(2);
+            return (
+              <tr key={ index }>
+                <td>{description}</td>
+                <td>{tag}</td>
+                <td>{method}</td>
+                <td>{value}</td>
+                <td>{exchangeRates[currency].name}</td>
+                <td>{rate}</td>
+                <td>{converted}</td>
+                <td>Real</td>
+                <td>--</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Form from '../components/Form';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
     return (
       <div>
         <Form />
+        <Table />
       </div>
     );
   }
