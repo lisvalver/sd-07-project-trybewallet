f498ba38c4e5218d28cfca91f55c90501827f456
MoisesSantana <moisaant@gmail.com>
2021-01-15 12:45:52 -0300

    req/5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -41,7 +41,7 @@ class Wallet extends React.Component {
   }
 
   render() {
-    const { email, totalExpense = 0 } = this.props;
+    const { expenses, email, totalExpense = 0 } = this.props;
 
     const {
       value,
@@ -148,6 +148,54 @@ class Wallet extends React.Component {
             Adicionar despesa
           </button>
         </fieldset>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            {expenses.map((expense) => (
+              <tr key={ expense.id }>
+                <td>
+                  {expense.description}
+                </td>
+                <td>
+                  {expense.tag}
+                </td>
+                <td>
+                  {expense.method}
+                </td>
+                <td>
+                  {expense.value}
+                </td>
+                <td>
+                  {expense.exchangeRates[expense.currency].name}
+                </td>
+                <td>
+                  {(expense.exchangeRates[expense.currency].ask
+                  * expense.value).toFixed(2)}
+                </td>
+                <td>
+                  {Number(expense.exchangeRates[expense.currency].ask).toFixed(2)}
+                </td>
+                <td>
+                  Real
+                </td>
+                <button data-testid="delete-btn" type="submit">Apagar</button>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </main>
     );
   }
@@ -157,6 +205,7 @@ const mapStateToProps = (state) => ({
   currencies: state.wallet.currencies,
   email: state.user.email,
   totalExpense: state.wallet.totalExpense,
+  expenses: state.wallet.expenses,
 });
 
 const mapDispatchToProps = (dispatch) => ({
@@ -170,6 +219,7 @@ Wallet.propTypes = {
   totalExpense: PropTypes.number.isRequired,
   currencies: PropTypes.arrayOf(PropTypes.object).isRequired,
   saveNewExpensive: PropTypes.func.isRequired,
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
