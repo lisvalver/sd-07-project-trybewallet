4d547198cd3be245652cf219246f1c250ab740af
Raphael Caputo <faelelf2@gmail.com>
2021-01-19 16:46:26 -0300

    Requisito 1 e 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,110 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      btnDisable: true,
+      email: '',
+      senha: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput({ target: { name, value } }) {
+    this.setState(
+      {
+        [name]: value,
+      },
+      () => this.inputsCheck(),
+    );
+  }
+
+  inputsCheck() {
+    const email = document.getElementById('email-input').value;
+    const senha = document.getElementById('password-input').value;
+    const regex = /^\w+@[a-zA-Z_]+?.[a-zA-Z]{2,3}$/;
+    const minCaracteres = 6;
+    if (senha.length === minCaracteres && regex.test(email)) {
+      this.setState({
+        btnDisable: false,
+        email,
+        senha,
+      });
+    } else {
+      this.setState({
+        btnDisable: true,
+      });
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { email } = this.state;
+    const { sendLogin, history } = this.props;
+    sendLogin(email);
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, btnDisable, senha } = this.state;
+    return (
+      <div className="container-login">
+        <div className="name-login">
+          <label htmlFor="email-input">
+            Email:
+            <input
+              id="email-input"
+              type="email"
+              name="email"
+              data-testid="email-input"
+              placeholder="E-mail"
+              required
+              onChange={ this.handleInput }
+              value={ email }
+            />
+          </label>
+        </div>
+        <div className="login-box">
+          <label htmlFor="password">
+            Senha:
+            <input
+              id="password-input"
+              type="password"
+              name="senha"
+              data-testid="password-input"
+              placeholder="senha"
+              required
+              onChange={ this.handleInput }
+              value={ senha }
+            />
+          </label>
+        </div>
+        <button
+          type="submit"
+          data-testid="my-action"
+          disabled={ btnDisable }
+          onClick={ this.handleSubmit }
+          className="btn"
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  sendLogin: (email) => dispatch(login(email)),
+});
+
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  btnDisable: PropTypes.bool.isRequired,
+}.isRequired;
+export default connect(null, mapDispatchToProps)(Login);
