ab89ed2fd4c5e991e3d70a7d4bf6d48dc0913c66
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2021-01-19 00:36:15 -0300

    eslint routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
