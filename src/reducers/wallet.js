1ec1ee235fb1ff94aad6ab75396d9b1bdf38d6d4
Bruno Dantas <bruno.wesley77@gmail.com>
2021-01-20 15:40:22 -0300

    requeriment 4

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,3 +1,5 @@
+import { CURRENCIES, ADD_EXPENSE } from '../actions';
+
 const INITIAL_STATE = {
   currencies: [],
   expenses: [],
@@ -7,6 +9,10 @@ export default function (state = INITIAL_STATE, action) {
   switch (action.type) {
   case '':
     return;
+  case CURRENCIES:
+    return { ...state, currencies: action.currencies };
+  case ADD_EXPENSE:
+    return { ...state, expenses: [...state.expenses, action.expense] };
   default:
     return state;
   }
