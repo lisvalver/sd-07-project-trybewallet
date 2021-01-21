e661346279a7a0790bed82d1151d1f6266e5ec74
Elisa <elisatcamillo@gmail.com>
2021-01-21 14:44:20 -0300

    req 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,108 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { addEmail } from '../actions/index';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      validEmail: false,
+      validPassword: false,
+      isDisabled: true,
+      email: '',
+    };
+
+    this.validateEmail = this.validateEmail.bind(this);
+    this.validatePassword = this.validatePassword.bind(this);
+  }
+
+  validateEmail(e) {
+    const email = e.target.value;
+
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+      .test(email)) {
+      this.setState({
+        validEmail: true,
+        email,
+      }, this.toggleButton);
+    } else {
+      this.setState({
+        validEmail: false,
+      }, this.toggleButton);
+    }
+  }
+
+  validatePassword(e) {
+    const password = e.target.value;
+    const minLength = 6;
+    if (password.length >= minLength) {
+      this.setState({
+        validPassword: true,
+      }, this.toggleButton);
+    } else {
+      this.setState({
+        validPassword: false,
+      }, this.toggleButton);
+    }
+  }
+
+  toggleButton() {
+    const { validEmail, validPassword } = this.state;
+    const disabledState = validEmail && validPassword;
+    this.setState({
+      isDisabled: !disabledState,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { isDisabled } = this.state;
+    const { addEmailAction } = this.props;
+    const { email } = this.state;
+    return (
+      <form>
+        <label htmlFor="email">
+          Email
+          <input
+            type="text"
+            id="email"
+            data-testid="email-input"
+            onChange={ this.validateEmail }
+          />
+        </label>
+
+        <label htmlFor="password">
+          Senha
+          <input
+            type="text"
+            id="password"
+            data-testid="password-input"
+            onChange={ this.validatePassword }
+          />
+        </label>
+
+        <Link to="/carteira">
+          <button
+            disabled={ isDisabled }
+            type="button"
+            onClick={ () => addEmailAction(email) }
+          >
+            Entrar
+          </button>
+        </Link>
+      </form>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  addEmailAction: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  addEmailAction: (email) => dispatch(addEmail(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <div>
+        <header>
+          <p data-testid="email-field">
+            {email}
+          </p>
+          <p data-testid="total-field">
+            0
+          </p>
+          <p data-testid="header-currency-field">
+            BRL
+          </p>
+        </header>
+
+        <form>
+          <input data-testid="value-input" />
+          <input data-testid="description-input" />
+          {/* <select data-testid="currency-input" >
+        <option>
+
+        </option>
+      </select> */}
+        </form>
+      </div>
+
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
