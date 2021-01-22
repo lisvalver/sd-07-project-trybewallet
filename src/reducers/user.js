c394f04980579b1eab8ae9c534225c66f1ba33fd
gabrieldeori <deori.pessoal@gmail.com>
2021-01-22 16:56:40 -0300

    Implemented store and reducers

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,14 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const user = (state = INITIAL_STATE, action) => state;
+
+export default user;
