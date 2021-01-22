4d846c3ca811f70a79af5dd167e3176c3db49208
gabrieldeori <deori.pessoal@gmail.com>
2021-01-22 09:38:47 -0300

    Alt Ordem dos imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import { Route, Switch } from 'react-router-dom';
 import React from 'react';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
-import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
