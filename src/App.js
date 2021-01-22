53fe9d7d67699c2f9043d31381c2c9e3ea2e0077
gabrieldeori <deori.pessoal@gmail.com>
2021-01-22 09:37:40 -0300

    Implementação inicial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div>
+      Hello, TrybeWallet!
+      <Switch>
+        <Route exact path="/"><Login /></Route>
+        <Route path="/wallet"><Wallet /></Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
