670b9acef4246fbc6f68c51d513cb7bc0ede82ed
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2021-01-23 15:27:26 -0300

    criando página de login e css

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import "./App.css";
 
 ReactDOM.render(
   <BrowserRouter>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React from "react";
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      disabled: true,
+    }
+  }
   render() {
-    return <div>Login</div>;
+    const {disabled} = this.state;
+    return (
+      <div>
+        <form className="form">
+          <h3>TRYBE WALLET</h3>
+          <input
+            type="text"
+            data-testid="email-input"
+            className="input-login"
+            placeholder="insira aqui o seu e-mail"
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+            className="input-login"
+            placeholder="insira aqui a sua senha"
+          />
+          <button className="button-login" disabled={disabled}>Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
