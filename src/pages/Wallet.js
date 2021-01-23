d22068219fe1484bf69ab25f8b2d41d2e56e3fc6
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2021-01-23 19:39:04 -0300

    requisto 3

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Header from '../components/Header';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+    <div>
+      <Header email={email} />
+    </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = ({ user: { email } }) => ({ email });
+
+export default connect(mapStateToProps)(Wallet);
