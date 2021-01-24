56c5f5b1e8cd7bd0472c4c42b05b7e374783484c
Lucas <lafdesouza2002@outlook.com>
2021-01-24 20:12:53 -0300

    Requisito 3 fluiu tbm

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      totalField: 0,
+      currencyField: 'BRL',
+    };
+  }
+
   render() {
-    return <div>carteira</div>;
+    const { email } = this.props;
+    const { totalField, currencyField } = this.state;
+    return (
+      <div>
+        <header>
+          <p data-testid="email-field">{email}</p>
+          <p data-testid="total-field">{totalField}</p>
+          <p data-testid="header-currency-field">{currencyField}</p>
+        </header>
+      </div>);
   }
 }
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
 });
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
 
 export default connect(mapStateToProps)(Wallet);
