7813af02482bbbe20f31a845fec6a61ccccdb480
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2021-01-24 22:02:04 -0300

    requisito 5

---
+++ src/components/ExpensesTable.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+class ExpensesTable extends React.Component {
+  constructor() {
+    super();
+
+    this.getExpenses = this.getExpenses.bind(this);
+  }
+
+  getExpenses(expense) {
+    const {
+      description,
+      value,
+      currency,
+      method,
+      tag,
+      exchangeRates,
+      id,
+    } = expense;
+    const nameCurrency = Object.values(exchangeRates).find(
+      (item) => item.code === currency
+    );
+    const { name, ask } = nameCurrency;
+    return (
+      <div key={id} className='content'>
+        <div role='cell'>{description}</div>
+        <div role='cell'>{tag}</div>
+        <div role='cell'>{method}</div>
+        <div role='cell'>{value}</div>
+        <div role='cell'>{name}</div>
+        <div role='cell'>{parseFloat(ask).toFixed(2)}</div>
+        <div role='cell'>{(value * ask).toFixed(2)}</div>
+        <div role='cell'>Real</div>
+      </div>
+    );
+  }
+
+  render() {
+    const { expenses } = this.props;
+    return (
+      <div>
+        <div className='header-table'>
+          <div>Descrição</div>
+          <div>Tag</div>
+          <div>Método de pagamento</div>
+          <div>Valor</div>
+          <div>Moeda</div>
+          <div>Câmbio utilizado</div>
+          <div>Valor convertido</div>
+          <div>Moeda de conversão</div>
+          <div>Editar/Excluir</div>
+        </div>
+        <div className='content-table'>
+          {expenses.length === 0 ? (
+            <p>Você ainda não adicionou as suas despesas :(</p>
+          ) : (
+            expenses.map((expense) => this.getExpenses(expense))
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ExpensesTable;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { saveCurrencyExchange, addExpense } from '../actions';
 import Header from '../components/Header';
 import Form from '../components/Form';
-
+import ExpensesTable from '../components/ExpensesTable';
 class Wallet extends React.Component {
   constructor() {
     super();
@@ -12,16 +12,16 @@ class Wallet extends React.Component {
     this.filterCurrenciesInitials = this.filterCurrenciesInitials.bind(this);
     this.eventHandler = this.eventHandler.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.resetState = this.resetState.bind(this);
+    this.resetState = this.resetState.bind(this);        
 
     this.state = {
-      expense: {        
+      expense: {
         description: '',
         value: 0,
         currency: '',
         method: '',
-        tag: '',  
-        exchangeRates: {},      
+        tag: '',
+        exchangeRates: {},
       },
     };
   }
@@ -34,8 +34,8 @@ class Wallet extends React.Component {
   filterCurrenciesInitials() {
     const { currencies } = this.props;
     const keysCurrencies = Object.keys(currencies).filter(
-      (currency) => currency !== 'USDT'
-    );    
+      (currency) => currency !== "USDT"
+    );
 
     return keysCurrencies.map((coin) => (
       <option key={coin} data-testid={coin}>
@@ -63,7 +63,7 @@ class Wallet extends React.Component {
         value: 0,
         currency: '',
         method: '',
-        tag: '',  
+        tag: '',
         exchangeRates: {},
       },
     }));
@@ -71,45 +71,52 @@ class Wallet extends React.Component {
 
   async handleSubmit() {
     const { addExpense } = this.props;
-    const { expense } = this.state;    
+    const { expense } = this.state;
     await addExpense(expense);
     this.resetState();
-  }
+  }  
 
   render() {
-    const { email, total } = this.props;
+    const { email, total, expenses } = this.props;
     const { description, value, currency, method, tag } = this.state.expense;
     return (
       <div>
-        <Header email={ email } total={ total } />
+        <Header email={email} total={total} />
         <Form
-          currencies={ this.filterCurrenciesInitials }
-          setState={ this.eventHandler }
-          onSubmit={ this.handleSubmit}
-          description={ description }
-          value={ value }
-          currency={ currency }
-          method={ method }
-          tag={ tag }
+          currencies={this.filterCurrenciesInitials}
+          setState={this.eventHandler}
+          onSubmit={this.handleSubmit}
+          description={description}
+          value={value}
+          currency={currency}
+          method={method}
+          tag={tag}
         />
+        <ExpensesTable expenses={expenses} />
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ user: { email }, wallet: { currencies, total } }) => ({
+const mapStateToProps = ({
+  user: { email },
+  wallet: { currencies, expenses, total },
+}) => ({
   email,
-  currencies, 
-  total, 
+  currencies,
+  expenses,
+  total,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   requestCoins: () => dispatch(saveCurrencyExchange()),
-  addExpense: (expense) => dispatch(addExpense(expense)), 
+  addExpense: (expense) => dispatch(addExpense(expense)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
 
 Wallet.propTypes = {
   email: PropTypes.string.isRequired,
+  currencies: PropTypes.object.isRequired,
+  total: PropTypes.number.isRequired,
 };
