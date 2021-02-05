9e27683909fd00f538c059f1985fd7bc0868d7e8
Breenolf <blf@ic.ufal.br>
2021-02-05 12:34:04 -0300

    Implementa Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Header extends React.Component {
+  render() {
+    const { userEmail } = this.props;
+    return (
+      <header>
+        <span data-testid="email-field">
+          { userEmail }
+        </span>
+        <span data-testid="total-field">
+          0
+        </span>
+        <span data-testid="header-currency-field">
+          BRL
+        </span>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
