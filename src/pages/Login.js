d7c3fb2e8d9e970e5e5ef7ec4a34c8b43f9910c4
Isabella Paz <bellapaz.iorp@gmail.com>
2021-02-07 19:19:16 -0300

    condiçao de ativacao do botao Entrar

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.sendAccessData = this.sendAccessData.bind(this);
+    this.checkLoginData = this.checkLoginData.bind(this);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  sendAccessData({ target }) {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  checkLoginData() {
+    const { email, password } = this.state;
+    const regexEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    const validateEmail = regexEmail.test(email);
+    const passwordMinimumLengthRequired = 6;
+    const validatePassword = password.length > passwordMinimumLengthRequired;
+    if (validateEmail && validatePassword) {
+      return false;
+    }
+    return true;
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
       <div>
         <h1>TRYBE WALLET</h1>
@@ -12,24 +44,29 @@ class Login extends Component {
               type="text"
               id="email"
               name="email"
+              value={ email }
               data-testid="email-input"
               autoComplete="on"
+              onChange={ this.sendAccessData }
             />
           </label>
-          <label htmlFor="senha">
+          <label htmlFor="password">
             Senha:
             <input
               type="password"
-              id="senha"
-              name="senha"
-              data-testid="email-input"
+              id="password"
+              name="password"
+              value={ password }
+              data-testid="password-input"
               autoComplete="on"
+              onChange={ this.sendAccessData }
             />
           </label>
           <button
-            type="button"
+            type="submit"
             name="Entrar"
             data-testid="login-submit-btn"
+            disabled={ this.checkLoginData }
           >
             Entrar
           </button>
