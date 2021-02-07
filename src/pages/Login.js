b4cafaac726d9a128788cc25f48b09194ab7a0d3
Isabella Paz <bellapaz.iorp@gmail.com>
2021-02-07 06:31:05 -0300

    construçao da base da pagina de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Login extends React.Component {
+class Login extends Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h1>TRYBE WALLET</h1>
+        <form id="loginForm">
+          <label htmlFor="email">
+            E-mail:
+            <input
+              type="text"
+              id="email"
+              name="email"
+              data-testid="email-input"
+              autoComplete="on"
+            />
+          </label>
+          <label htmlFor="senha">
+            Senha:
+            <input
+              type="password"
+              id="senha"
+              name="senha"
+              data-testid="email-input"
+              autoComplete="on"
+            />
+          </label>
+          <button
+            type="button"
+            name="Entrar"
+            data-testid="login-submit-btn"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
