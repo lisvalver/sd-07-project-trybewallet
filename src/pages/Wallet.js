d2e73705fed8470a8cd237475b8373496333d210
Alstent <rafaelasltent@gmail.com>
2021-02-08 22:15:55 +0100

    resolvendo erro requisto 4

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -86,7 +86,7 @@ class Wallet extends React.Component {
               name="value"
               data-testid="value-input"
               onChange={ this.inputOnChange }
-              placeholder="0"
+              value={expenses.value}
             />
           </label>
           <label htmlFor="description">
