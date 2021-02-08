ab188df4e0dda572994b3665848fcbf69c072aa2
Isabella Paz <bellapaz.iorp@gmail.com>
2021-02-08 02:36:19 -0300

    campo para adicionar o valor da nova despesa

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,30 +1,62 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-class Header extends Component {
+class Formulario extends Component {
+  constructor(props) {
+    super(props);
+
+    this.sendNewExpenseInfos = this.sendNewExpenseInfos.bind(this);
+
+    this.state = {
+      newExpenseValue: 0,
+    };
+  }
+
+  sendNewExpenseInfos({ target }) {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
     // const { wallet } = this.props;
     // const { currencies, expenses } = wallet;
+    const { newExpenseValue } = this.state;
     return (
       <div>
-        <h2>Adicionar Nova Despesa</h2>
+        <form id="addExpensesForm">
+          <h2>Adicionar Nova Despesa</h2>
+          <label htmlFor="newExpenseValue">
+            Valor da Despesa:
+            <input
+              type="number"
+              id="newExpenseValue"
+              name="newExpenseValue"
+              step="0.01"
+              min="0"
+              value={ newExpenseValue }
+              data-testid="value-input"
+              onChange={ this.sendNewExpenseInfos }
+            />
+          </label>
+        </form>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-  wallet: state.wallet,
-});
+// const mapStateToProps = (state) => ({
+//   wallet: state.wallet,
+// });
 
-Header.propTypes = {
-  user: PropTypes.shape({
-    email: PropTypes.string.isRequired,
-  }).isRequired,
+Formulario.propTypes = {
   wallet: PropTypes.shape({
     expenses: PropTypes.arrayOf.isRequired,
   }).isRequired,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Formulario);
+// export default connect(mapStateToProps, mapDispatchToProps)(Formulario);
+
+export default Formulario;
