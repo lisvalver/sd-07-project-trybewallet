a97a6f49b23e80e4aa3faea966585a2d029cff65
Isabella Paz <bellapaz.iorp@gmail.com>
2021-02-08 00:26:22 -0300

    Exibiçao do email do usuario no Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { user } = this.props;
+    const { email } = user;
     return (
       <div>
-        <h1>Header</h1>
+        <header className="header">
+          <h1>TRYBE WALLET</h1>
+          <div>
+            <h3>Email: </h3>
+            <span data-testid="email-field">{ email }</span>
+          </div>
+        </header>
       </div>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+Header.propTypes = {
+  user: PropTypes.shape({
+    email: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
