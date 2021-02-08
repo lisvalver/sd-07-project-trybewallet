596dd62d84f5d04d91190c473fe96b862df53bb6
Isabella Paz <bellapaz.iorp@gmail.com>
2021-02-08 09:42:54 -0300

    criaçao das actions para requisiçao das currencies, componente para escolher currency e condicao isFetching

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import apiCurrenciesDispatch from '../actions';
 
 class Formulario extends Component {
   constructor(props) {
@@ -11,7 +12,13 @@ class Formulario extends Component {
     this.state = {
       newExpenseValue: 0,
       expenseDescription: '',
+      selectedCurrency: '',
     };
+  }
+
+  componentDidMount() {
+    const { apiCurrencies } = this.props;
+    apiCurrencies();
   }
 
   sendNewExpenseInfos({ target }) {
@@ -22,54 +29,87 @@ class Formulario extends Component {
   }
 
   render() {
-    // const { wallet } = this.props;
-    // const { currencies, expenses } = wallet;
-    const { newExpenseValue, expenseDescription } = this.state;
+    const { wallet } = this.props;
+    const {
+      currencies,
+      // expenses,
+      isFetching,
+      // error,
+    } = wallet;
+    const { newExpenseValue, expenseDescription, selectedCurrency } = this.state;
     return (
       <div>
-        <form id="addExpensesForm">
-          <h2>Adicionar Nova Despesa</h2>
-          <label htmlFor="newExpenseValue">
-            Valor da Despesa:
-            <input
-              type="number"
-              id="newExpenseValue"
-              name="newExpenseValue"
-              step="0.01"
-              min="0"
-              value={ newExpenseValue }
-              data-testid="value-input"
-              onChange={ this.sendNewExpenseInfos }
-            />
-          </label>
-          <br />
-          <label htmlFor="expenseDescription">
-            Valor da Despesa:
-            <input
-              type="text"
-              id="expenseDescription"
-              name="expenseDescription"
-              value={ expenseDescription }
-              data-testid="description-input"
-              onChange={ this.sendNewExpenseInfos }
-            />
-          </label>
-        </form>
+        {isFetching ? <p>Carregando...</p>
+          : (
+            <form id="addExpensesForm">
+              <h2>Adicionar Nova Despesa</h2>
+              <label htmlFor="newExpenseValue">
+                Valor da Despesa:
+                <input
+                  type="number"
+                  id="newExpenseValue"
+                  name="newExpenseValue"
+                  step="0.01"
+                  min="0"
+                  value={ newExpenseValue }
+                  data-testid="value-input"
+                  onChange={ this.sendNewExpenseInfos }
+                />
+              </label>
+              <br />
+              <label htmlFor="expenseDescription">
+                Valor da Despesa:
+                <input
+                  type="text"
+                  id="expenseDescription"
+                  name="expenseDescription"
+                  value={ expenseDescription }
+                  data-testid="description-input"
+                  onChange={ this.sendNewExpenseInfos }
+                />
+              </label>
+              <br />
+              <label htmlFor="currency">
+                Moeda:
+                <select
+                  id="currency"
+                  name="currency"
+                  data-testid="currency-input"
+                  value={ selectedCurrency }
+                  onChange={ this.sendNewExpenseInfos }
+                >
+                  {currencies.map((currency) => (
+                    <option
+                      key={ currency }
+                      data-testid={ `${currency}` }
+                    >
+                      {currency}
+                    </option>))}
+                </select>
+              </label>
+            </form>
+          )}
       </div>
     );
   }
 }
 
-// const mapStateToProps = (state) => ({
-//   wallet: state.wallet,
-// });
+const mapStateToProps = (state) => ({
+  wallet: state.wallet,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  apiCurrencies: () => dispatch(apiCurrenciesDispatch()),
+});
 
 Formulario.propTypes = {
   wallet: PropTypes.shape({
     expenses: PropTypes.arrayOf.isRequired,
+    currencies: PropTypes.arrayOf.isRequired,
+    isFetching: PropTypes.bool.isRequired,
+    error: PropTypes.string,
   }).isRequired,
+  apiCurrencies: PropTypes.func.isRequired,
 };
 
-// export default connect(mapStateToProps, mapDispatchToProps)(Formulario);
-
-export default Formulario;
+export default connect(mapStateToProps, mapDispatchToProps)(Formulario);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -3,12 +3,18 @@ import types from '../actions/types';
 const WALLET_INITIAL_VALUE = {
   currencies: [],
   expenses: [],
+  isFetching: false,
+  error: '',
 };
 
 const wallet = (state = WALLET_INITIAL_VALUE, action) => {
   switch (action.type) {
-  case types.ANY:
-    return state;
+  case types.CURRENCIES_REQUEST:
+    return { ...state, isFetching: true };
+  case types.CURRENCIES_REQUEST_FAILED:
+    return { ...state, error: action.error, isFetching: false };
+  case types.CURRENCIES_REQUEST_SUCCEED:
+    return { ...state, currencies: action.currencies, isFetching: false };
   default:
     return state;
   }
