eb4a858d3513c779e118a37b501c23361eb8440a
MarceloBorges <melombm@hotmail.com>
2021-02-09 17:10:44 -0500

    correçao de PropTypes

--- src/components/FormCart.jsx
+++ src/components/FormCart.jsx
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
 
 class FormCart extends Component {
+  componentDidMount() {
+    fetch('https://economia.awesomeapi.com.br/json/all')
+      .then((response) => response.json())
+      .then((jsonResponse) => console.log(jsonResponse));
+  }
+
   render() {
     return (
       <div>
         <form>
-          formulario
+          <label htmlFor="value">
+            Despensa:
+            <input
+              id="value"
+              type="text"
+              data-testid="value-input"
+            />
+          </label>
+
+          <label htmlFor="description">
+            Valor
+            <input
+              id="description"
+              type="text"
+              data-testid="description-input"
+            />
+          </label>
+          <span>Moeda:</span>
+          <select
+            data-testid="currency-input"
+          >
+            <option>
+              USD
+            </option>
+          </select>
+
         </form>
       </div>
     );
   }
 }
+
 export default FormCart;
