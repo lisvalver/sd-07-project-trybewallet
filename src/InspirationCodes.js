0ff7b9692fe76e3ee66e156031420d434b0d8eb1
gustasaideles <gustasaideles.gmail.com>
2021-02-10 10:47:10 -0300

    dando creditos aos criadores dos codigos

---
+++ src/InspirationCodes.js
@@ -0,0 +1,6 @@
+/* Códigos ispirados e/ou copiados nos projetos dos colegas:
+https://github.com/tryber/sd-07-project-trybewallet/pull/73
+https://github.com/tryber/sd-07-project-trybewallet/pull/11
+https://github.com/tryber/sd-07-project-trybewallet/pull/81
+porém sei explicar cada um deles
+*/
