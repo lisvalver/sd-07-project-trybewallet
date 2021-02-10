d10eb535947287e9b9c0ffa97e6a687fb01166c0
Breenolf <blf@ic.ufal.br>
2021-02-09 22:46:13 -0300

    atualiza header 3

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,7 +15,7 @@ class Header extends React.Component {
           { `Email: ${userEmail}` }
         </span>
         <span data-testid="total-field">
-          { `Despesa Total: R$ ${parseFloat(amount)}` }
+          { parseFloat(amount) }
         </span>
         <span data-testid="header-currency-field">
           BRL
