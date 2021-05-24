5896820732b2860e8be351fcf3e76a69f54b8e8e
Éric Vinícius <ericcobain3@gmail.com>
2021-05-23 21:16:56 -0300

    hoc with axios

---
+++ src/hocComponentes/hocWithAxios.js
@@ -0,0 +1,12 @@
+// const MyComponent = withAxios(class MyComponentRaw extends React.Component {
+//   componentWillMount() {
+//     this.props.axios('test').then((result) => {
+//       this.setState({ data: result.data });
+//     });
+//   }
+
+//   render() {
+//     const { data } = this.state || {};
+//     return <div>{JSON.stringify(data)}</div>;
+//   }
+// });
