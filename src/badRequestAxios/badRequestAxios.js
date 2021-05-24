5bc0963b75711234128ddc459e673526cc9ff2cd
Éric Vinícius <ericcobain3@gmail.com>
2021-05-23 21:24:19 -0300

    bad request axios

---
+++ src/badRequestAxios/badRequestAxios.js
@@ -0,0 +1,14 @@
+// {/* <Request
+//   instance={axios.create({})} /* custom instance of axios - optional */
+//   method="" /* get, delete, head, post, put and patch - required */
+//   url="" /*  url endpoint to be requested - required */
+//   data={} /* post data - optional */
+//   params={} /* queryString data - optional */
+//   config={} /* axios config - optional */
+//   debounce={200} /* minimum time between requests events - optional */
+//   debounceImmediate={true} /* make the request on the beginning or trailing end of debounce - optional */
+//   isReady={true} /* can make the axios request - optional */
+//   onSuccess={(response)=>{}} /* called on success of axios request - optional */
+//   onLoading={()=>{}} /* called on start of axios request - optional */
+//   onError=(error)=>{} /* called on error of axios request - optional */
+// /> */}
